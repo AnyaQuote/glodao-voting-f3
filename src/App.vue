3b732bceab9ce49a435b996f7fffbe2158bc2eea
duchm22 <duchm22.se@gmail.com>
2022-05-13 06:30:25 +0700

    feat: add app bar and footer to page

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <v-app>
-    <v-main>
+    <glodao-app-bar app class="neutral100" />
+    <v-main app class="neutral15">
       <router-view></router-view>
     </v-main>
+    <glodao-footer app />
   </v-app>
 </template>
 
 <script>
-import { Observer } from "mobx-vue"
-import { Component, Vue } from "vue-property-decorator"
-import { appProvider } from "./app-providers"
-import { walletStore } from "./stores/wallet-store"
+import { Observer } from 'mobx-vue'
+import { Component, Vue } from 'vue-property-decorator'
+import { appProvider } from './app-providers'
+import { walletStore } from './stores/wallet-store'
 
 @Observer
 @Component({
-  components: {}
+  components: {
+    'glodao-app-bar': () => import('@/components/glodao-app-bar.vue'),
+    'glodao-footer': () => import('@/components/glodao-footer.vue')
+  }
 })
 export default class App extends Vue {
   mounted() {
@@ -23,3 +28,15 @@ export default class App extends Vue {
   }
 }
 </script>
+
+<style lang="scss">
+@import './styles/variables.scss';
+
+.debug {
+  border: 1px solid red !important;
+}
+
+.container {
+  max-width: 1092px !important;
+}
+</style>
