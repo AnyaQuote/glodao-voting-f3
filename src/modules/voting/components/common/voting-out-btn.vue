bdcdecfb3770dd03305779f2652d9e46e5c59250
duchm22 <duchm22.se@gmail.com>
2022-05-13 06:29:14 +0700

    feat: create custom component

---
+++ src/modules/voting/components/common/voting-out-btn.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn v-bind="$attrs" v-on="$listeners" icon class="rounded-circle white px-2">
+    <v-img contain src="@/assets/icons/voting-home--arrow-out.svg" />
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class VotingOutBtn extends Vue {}
+</script>
+
+<style scoped></style>
