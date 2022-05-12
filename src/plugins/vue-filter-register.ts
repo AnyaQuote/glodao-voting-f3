10a3db70181df0c82bace09be8a4fddb45862229
duchm22 <duchm22.se@gmail.com>
2022-05-13 06:25:06 +0700

    chore: replace double quote with single quote

--- src/plugins/vue-filter-register.ts
+++ src/plugins/vue-filter-register.ts
@@ -1,70 +1,70 @@
-import moment from "moment"
-import Vue from "vue"
-import { get, isNumber, isEmpty, round } from "lodash-es"
-import { FixedNumber } from "@ethersproject/bignumber"
+import moment from 'moment'
+import Vue from 'vue'
+import { get, isNumber, isEmpty, round } from 'lodash-es'
+import { FixedNumber } from '@ethersproject/bignumber'
 
 export const vueFilterRegister = () => {
-  Vue.filter("date", (isoStr: string, format: string) => (isoStr ? moment(isoStr).format(format) : ""))
-  Vue.filter("round", (number: any, precision = 5) => {
-    if (typeof number === "number") return number ? round(number, precision) : "0"
+  Vue.filter('date', (isoStr: string, format: string) => (isoStr ? moment(isoStr).format(format) : ''))
+  Vue.filter('round', (number: any, precision = 5) => {
+    if (typeof number === 'number') return number ? round(number, precision) : '0'
     if (number instanceof FixedNumber) {
       const rouned = number.round(precision).toString()
-      return rouned.endsWith(".0") ? rouned.replace(".0", "") : rouned
+      return rouned.endsWith('.0') ? rouned.replace('.0', '') : rouned
     }
     return number
   })
-  Vue.filter("datetime", (isoStr: string, format = "lll") => (isoStr ? moment(isoStr).format(format) : ""))
-  Vue.filter("ddmmyyyy", (isoStr: string) => (isoStr ? moment(isoStr).format("DD/MM/YYYY") : ""))
-  Vue.filter("ddmmyyyyhhmmss", (isoStr: string) => (isoStr ? moment(isoStr).format("DD/MM/YYYY HH:mm:ss") : ""))
-  Vue.filter("truncateAddress", (address = "", start = 5, end = 4) => {
-    if (!address) return ""
-    return address.substr(0, start) + "..." + address.substr(address.length - end, end)
+  Vue.filter('datetime', (isoStr: string, format = 'lll') => (isoStr ? moment(isoStr).format(format) : ''))
+  Vue.filter('ddmmyyyy', (isoStr: string) => (isoStr ? moment(isoStr).format('DD/MM/YYYY') : ''))
+  Vue.filter('ddmmyyyyhhmmss', (isoStr: string) => (isoStr ? moment(isoStr).format('DD/MM/YYYY HH:mm:ss') : ''))
+  Vue.filter('truncateAddress', (address = '', start = 5, end = 4) => {
+    if (!address) return ''
+    return address.substr(0, start) + '...' + address.substr(address.length - end, end)
   })
 
-  Vue.filter("_get", (any: any, path: string, defaultValue = "") => {
+  Vue.filter('_get', (any: any, path: string, defaultValue = '') => {
     return get(any, path, defaultValue)
   })
-  Vue.filter("_empty", (any: any) => {
+  Vue.filter('_empty', (any: any) => {
     return !isNumber(any) && (!any || isEmpty(any))
   })
-  Vue.filter("_hasValue", (any: any) => {
+  Vue.filter('_hasValue', (any: any) => {
     return isNumber(any) || (any && !isEmpty(any))
   })
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
     minimumFractionDigits: 0
   })
-  Vue.filter("usd", (number: any) => {
+  Vue.filter('usd', (number: any) => {
     if (+number === 0) return `$${number}`
     if (!isNaN(+number) && +number !== 0) return formatter.format(+number)
     return number
   })
-  Vue.filter("formatNumber", (number: any, maximumFractionDigits = 5) => {
-    const nf = new Intl.NumberFormat("en-US", {
+  Vue.filter('formatNumber', (number: any, maximumFractionDigits = 5) => {
+    const nf = new Intl.NumberFormat('en-US', {
       maximumFractionDigits: maximumFractionDigits
     })
     return nf.format(number)
   })
-  Vue.filter("twoDigits", value => {
+  Vue.filter('twoDigits', value => {
     if (value < 0) {
-      return "00"
+      return '00'
     }
     if (value.toString().length <= 1) {
       return `0${value}`
     }
     return value
   })
-  Vue.filter("tribe", value => {
+  Vue.filter('tribe', value => {
     switch (value) {
-      case "Water":
-        return "Merman"
-      case "Wood":
-        return "Treant"
-      case "Ghost":
-        return "Shadow"
-      case "Elf":
-        return "Angel"
+      case 'Water':
+        return 'Merman'
+      case 'Wood':
+        return 'Treant'
+      case 'Ghost':
+        return 'Shadow'
+      case 'Elf':
+        return 'Angel'
       default:
         return value
     }
--- src/stores/wallet-store.ts
+++ src/stores/wallet-store.ts
@@ -1,24 +1,24 @@
-import { localdata } from "./../helpers/local-data"
-import { action, computed, observable, runInAction } from "mobx"
-import { actionAsync, asyncAction } from "mobx-utils"
-import Application from "@/libs/models"
-import Web3 from "web3"
-import { Subscription, timer } from "rxjs"
-import { loadingController } from "@/components/global-loading/global-loading-controller"
-import { Zero } from "@/constants"
-import { FixedNumber } from "@ethersproject/bignumber"
-import { snackController } from "@/components/snack-bar/snack-bar-controller"
-import { bnHelper } from "@/helpers/bignumber-helper"
+import { localdata } from './../helpers/local-data'
+import { action, computed, observable, runInAction } from 'mobx'
+import { actionAsync, asyncAction } from 'mobx-utils'
+import Application from '@/libs/models'
+import Web3 from 'web3'
+import { Subscription, timer } from 'rxjs'
+import { loadingController } from '@/components/global-loading/global-loading-controller'
+import { Zero } from '@/constants'
+import { FixedNumber } from '@ethersproject/bignumber'
+import { snackController } from '@/components/snack-bar/snack-bar-controller'
+import { bnHelper } from '@/helpers/bignumber-helper'
 
 export class WalletStore {
   ethereum: any = (window as any).ethereum
 
   app = new Application({ mainnet: false })
   @observable web3: Web3 | null = null
-  @observable account = ""
+  @observable account = ''
   @observable bnbBalance = Zero
   @observable z9Balance = Zero
-  @observable chainId = ""
+  @observable chainId = ''
   @observable loaded = false
 
   @observable navigationDrawer = false
@@ -34,11 +34,11 @@ export class WalletStore {
 
   @asyncAction *getBnbBalance() {
     const result = yield this.web3?.eth.getBalance(this.account!)
-    this.bnbBalance = FixedNumber.from(this.web3?.utils.fromWei(result, "ether"))
+    this.bnbBalance = FixedNumber.from(this.web3?.utils.fromWei(result, 'ether'))
   }
   @asyncAction *getUserLPBalance() {
     this.LPTokenContract = new (this.web3 as any).eth.Contract(
-      require("@/helpers/erc20.abi.json"),
+      require('@/helpers/erc20.abi.json'),
       process.env.VUE_APP_TOKEN_ADDRESS
     )
     const balance = yield this.LPTokenContract.methods.balanceOf(this.account!).call()
@@ -68,10 +68,10 @@ export class WalletStore {
         this.web3 = this.app.web3
         this.chainId = yield this.web3!.eth.getChainId()
         this.account = yield this.app.getAddress()
-        this.ethereum.removeListener("accountsChanged", this.ethereumConfigChanged)
-        this.ethereum.removeListener("chainChanged", this.ethereumConfigChanged)
-        this.ethereum.once("accountsChanged", this.ethereumConfigChanged)
-        this.ethereum.once("chainChanged", this.ethereumConfigChanged)
+        this.ethereum.removeListener('accountsChanged', this.ethereumConfigChanged)
+        this.ethereum.removeListener('chainChanged', this.ethereumConfigChanged)
+        this.ethereum.once('accountsChanged', this.ethereumConfigChanged)
+        this.ethereum.once('chainChanged', this.ethereumConfigChanged)
         if (this.isChainIdValid) {
           // this.getBnbBalance()
           // this.getUserLPBalance()
@@ -109,56 +109,59 @@ export class WalletStore {
     if (this.connected) {
       try {
         await this.ethereum.request({
-          method: "wallet_switchEthereumChain",
+          method: 'wallet_switchEthereumChain',
           params: [{ chainId: Web3.utils.toHex(chainId) }]
         })
       } catch (error) {
-        if (error.message.includes("wallet_addEthereumChain")) {
+        if (error.message.includes('wallet_addEthereumChain')) {
           if (chainId === Number(process.env.VUE_APP_CHAIN_ID)) {
             this.ethereum.request({
-              method: "wallet_addEthereumChain",
+              method: 'wallet_addEthereumChain',
               params: [
                 {
                   chainId: Web3.utils.toHex(chainId),
-                  chainName: "BSC Mainnet",
+                  chainName: 'BSC Mainnet',
                   nativeCurrency: {
-                    name: "BSC",
-                    symbol: "BNB",
+                    name: 'BSC',
+                    symbol: 'BNB',
                     decimals: 18
                   },
                   rpcUrls: [
-                    "https://bsc-dataseed1.binance.org",
-                    "https://bsc-dataseed2.binance.org",
-                    "https://bsc-dataseed3.binance.org",
-                    "https://bsc-dataseed4.binance.org",
-                    "https://bsc-dataseed1.defibit.io",
-                    "https://bsc-dataseed2.defibit.io",
-                    "https://bsc-dataseed3.defibit.io",
-                    "https://bsc-dataseed4.defibit.io",
-                    "https://bsc-dataseed1.ninicoin.io",
-                    "https://bsc-dataseed2.ninicoin.io",
-                    "https://bsc-dataseed3.ninicoin.io",
-                    "https://bsc-dataseed4.ninicoin.io",
-                    "wss://bsc-ws-node.nariox.org"
+                    'https://bsc-dataseed1.binance.org',
+                    'https://bsc-dataseed2.binance.org',
+                    'https://bsc-dataseed3.binance.org',
+                    'https://bsc-dataseed4.binance.org',
+                    'https://bsc-dataseed1.defibit.io',
+                    'https://bsc-dataseed2.defibit.io',
+                    'https://bsc-dataseed3.defibit.io',
+                    'https://bsc-dataseed4.defibit.io',
+                    'https://bsc-dataseed1.ninicoin.io',
+                    'https://bsc-dataseed2.ninicoin.io',
+                    'https://bsc-dataseed3.ninicoin.io',
+                    'https://bsc-dataseed4.ninicoin.io',
+                    'wss://bsc-ws-node.nariox.org'
                   ],
-                  blockExplorerUrls: ["https://bscscan.com"]
+                  blockExplorerUrls: ['https://bscscan.com']
                 }
               ]
             })
           } else if (Number(process.env.VUE_APP_CHAIN_ID)) {
             this.ethereum.request({
-              method: "wallet_addEthereumChain",
+              method: 'wallet_addEthereumChain',
               params: [
                 {
                   chainId: Web3.utils.toHex(chainId),
-                  chainName: "BSC Testnet",
+                  chainName: 'BSC Testnet',
                   nativeCurrency: {
-                    name: "BSC",
-                    symbol: "tBNB",
+                    name: 'BSC',
+                    symbol: 'tBNB',
                     decimals: 18
                   },
-                  rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545/", "https://data-seed-prebsc-2-s1.binance.org:8545/"],
-                  blockExplorerUrls: ["https://bscscan.com"]
+                  rpcUrls: [
+                    'https://data-seed-prebsc-1-s1.binance.org:8545/',
+                    'https://data-seed-prebsc-2-s1.binance.org:8545/'
+                  ],
+                  blockExplorerUrls: ['https://bscscan.com']
                 }
               ]
             })
@@ -174,13 +177,13 @@ export class WalletStore {
   }
 
   @computed get shortAccount() {
-    if (!this.account) return ""
-    return this.account.substr(0, 3) + "..." + this.account.substr(this.account.length - 3)
+    if (!this.account) return ''
+    return this.account.substr(0, 3) + '...' + this.account.substr(this.account.length - 3)
   }
 
   @computed get isChainIdValid() {
     //TODO: change to mainnet
-    return this.chainId && this.chainId + "" === Number(process.env.VUE_APP_CHAIN_ID).toString()
+    return this.chainId && this.chainId + '' === Number(process.env.VUE_APP_CHAIN_ID).toString()
     // return this.chainId && this.chainId + "" === Number(process.env.VUE_APP_CHAIN_ID).toString();
   }
 
