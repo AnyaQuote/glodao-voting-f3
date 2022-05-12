f906e05af27cf6c3a1abca8d9b435dc9466fbff5
duchm22 <duchm22.se@gmail.com>
2022-05-13 06:29:59 +0700

    feat: create container for voting pages

---
+++ src/modules/voting/container/container.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <router-view></router-view>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Container extends Vue {}
+</script>
+
+<style scoped></style>
