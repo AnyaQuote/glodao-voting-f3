dd9381b0feee073cd84fe27197bdd75bc0559cfd
duchm22 <duchm22.se@gmail.com>
2022-05-16 07:17:04 +0700

    feat: create regist container

---
+++ src/modules/regist/container/container.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <router-view></router-view>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class RegistContainer extends Vue {}
+</script>
+
+<style scoped></style>
