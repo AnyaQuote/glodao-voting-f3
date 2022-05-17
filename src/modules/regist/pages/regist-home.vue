fef2e45d953f9ad31d6c99d78368dccd65029f62
duchm22 <duchm22.se@gmail.com>
2022-05-17 07:49:42 +0700

    chore: remove whitespace

--- src/modules/regist/pages/regist-home.vue
+++ src/modules/regist/pages/regist-home.vue
@@ -1,81 +1,81 @@
 <template>
-    <v-tabs>
-      <v-tab>All</v-tab>
-      <v-tab>Bounty project</v-tab>
-      <v-tab>Launchpad project</v-tab>
-      <v-tab-item>
-        <div class="row pa-6">
-          <div class="col-3">
-            <v-select :items="['rejected', 'upcoming']" label="status"></v-select>
-          </div>
-          <div class="col-12">
-            <v-row>
-              <v-col cols="6" v-for="i in [1, 2, 3, 4, 5]" :key="i">
-                <regist-card />
-              </v-col>
-              <v-col cols="6">
-                <v-sheet
-                  class="neutral100--bg rounded-lg fill-height d-flex justify-center align-center cursor-pointer"
-                  v-ripple
-                  outlined
-                >
-                  <v-icon color="bluePrimary">mdi-plus</v-icon>
-                  <span class="font-weight-bold bluePrimary--text">New Application</span>
-                </v-sheet>
-              </v-col>
-            </v-row>
-          </div>
+  <v-tabs>
+    <v-tab>All</v-tab>
+    <v-tab>Bounty project</v-tab>
+    <v-tab>Launchpad project</v-tab>
+    <v-tab-item>
+      <div class="row pa-6">
+        <div class="col-3">
+          <v-select :items="['rejected', 'upcoming']" label="status"></v-select>
         </div>
-      </v-tab-item>
-      <v-tab-item>
-        <div class="row pa-6">
-          <div class="col-3">
-            <v-select :items="['rejected', 'upcoming']" label="status"></v-select>
-          </div>
-          <div class="col-12">
-            <v-row>
-              <v-col cols="6" v-for="i in [1, 2, 3, 4, 5]" :key="i">
-                <regist-card />
-              </v-col>
-              <v-col cols="6">
-                <v-sheet
-                  class="neutral100--bg rounded-lg fill-height d-flex justify-center align-center cursor-pointer"
-                  v-ripple
-                  outlined
-                >
-                  <v-icon color="bluePrimary">mdi-plus</v-icon>
-                  <span class="font-weight-bold bluePrimary--text">New Application</span>
-                </v-sheet>
-              </v-col>
-            </v-row>
-          </div>
+        <div class="col-12">
+          <v-row>
+            <v-col cols="6" v-for="i in [1, 2, 3, 4, 5]" :key="i">
+              <regist-card />
+            </v-col>
+            <v-col cols="6">
+              <v-sheet
+                class="neutral100--bg rounded-lg fill-height d-flex justify-center align-center cursor-pointer"
+                v-ripple
+                outlined
+              >
+                <v-icon color="bluePrimary">mdi-plus</v-icon>
+                <span class="font-weight-bold bluePrimary--text">New Application</span>
+              </v-sheet>
+            </v-col>
+          </v-row>
         </div>
-      </v-tab-item>
-      <v-tab-item>
-        <div class="row pa-6">
-          <div class="col-3">
-            <v-select :items="['rejected', 'upcoming']" label="status"></v-select>
-          </div>
-          <div class="col-12">
-            <v-row>
-              <v-col cols="6" v-for="i in [1, 2, 3, 4, 5]" :key="i">
-                <regist-card />
-              </v-col>
-              <v-col cols="6">
-                <v-sheet
-                  class="neutral100--bg rounded-lg fill-height d-flex justify-center align-center cursor-pointer"
-                  v-ripple
-                  outlined
-                >
-                  <v-icon color="bluePrimary">mdi-plus</v-icon>
-                  <span class="font-weight-bold bluePrimary--text">New Application</span>
-                </v-sheet>
-              </v-col>
-            </v-row>
-          </div>
+      </div>
+    </v-tab-item>
+    <v-tab-item>
+      <div class="row pa-6">
+        <div class="col-3">
+          <v-select :items="['rejected', 'upcoming']" label="status"></v-select>
         </div>
-      </v-tab-item>
-    </v-tabs>
+        <div class="col-12">
+          <v-row>
+            <v-col cols="6" v-for="i in [1, 2, 3, 4, 5]" :key="i">
+              <regist-card />
+            </v-col>
+            <v-col cols="6">
+              <v-sheet
+                class="neutral100--bg rounded-lg fill-height d-flex justify-center align-center cursor-pointer"
+                v-ripple
+                outlined
+              >
+                <v-icon color="bluePrimary">mdi-plus</v-icon>
+                <span class="font-weight-bold bluePrimary--text">New Application</span>
+              </v-sheet>
+            </v-col>
+          </v-row>
+        </div>
+      </div>
+    </v-tab-item>
+    <v-tab-item>
+      <div class="row pa-6">
+        <div class="col-3">
+          <v-select :items="['rejected', 'upcoming']" label="status"></v-select>
+        </div>
+        <div class="col-12">
+          <v-row>
+            <v-col cols="6" v-for="i in [1, 2, 3, 4, 5]" :key="i">
+              <regist-card />
+            </v-col>
+            <v-col cols="6">
+              <v-sheet
+                class="neutral100--bg rounded-lg fill-height d-flex justify-center align-center cursor-pointer"
+                v-ripple
+                outlined
+              >
+                <v-icon color="bluePrimary">mdi-plus</v-icon>
+                <span class="font-weight-bold bluePrimary--text">New Application</span>
+              </v-sheet>
+            </v-col>
+          </v-row>
+        </div>
+      </div>
+    </v-tab-item>
+  </v-tabs>
 </template>
 
 <script lang="ts">
