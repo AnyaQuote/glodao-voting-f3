f4f89f89cb8b186b630da26ac90d1f3d4a04e6e9
duchm22 <duchm.se22@gmail.com>
2022-05-17 11:15:16 +0700

    chore: update icon-wrapper

--- src/components/icon-wrapper.vue
+++ src/components/icon-wrapper.vue
@@ -1,21 +1,46 @@
 <template>
-    <v-avatar v-bind="$attrs" v-on="$listeners">
-        <v-img :src="source" />
-    </v-avatar>
+  <v-avatar v-if="iconFromLink" v-bind="$attrs" v-on="$listeners">
+    <v-img :src="source" />
+  </v-avatar>
+  <v-icon v-else v-bind="$attrs" v-on="$listeners" v-html="source"></v-icon>
 </template>
 
 <script lang="ts">
-    import { Component, Vue, Prop } from 'vue-property-decorator';
+import { URL } from 'url'
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
 
-    @Component
-    export default class  extends Vue {
-        @Prop(String) src!: string
-        get source() {
-            return this.src
-        }
+@Component
+export default class extends Vue {
+  @Prop(String) src!: string
+
+  iconFromLink = true
+  // source = ''
+
+  // @Watch('src', { immediate: true })
+  // onSrcChange(value) {
+  //   console.log(value)
+  //   this.source = value
+  // }
+
+  get source() {
+    if (this.src.includes('mdi') || this.src.includes('fa')) {
+      // is material design icon or fontawsome icon
+      this.iconFromLink = false
+      return this.src
+    } else {
+      this.iconFromLink = true
+      try {
+        // is absolute path (URL, https, http)
+        new URL(this.src)
+        return this.src
+      } catch (_) {
+        // is relative path
+        console.log('this.src:::', this.src)
+        return this.src
+      }
     }
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
