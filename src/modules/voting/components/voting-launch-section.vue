a478f22461281a1e0c57c20411887f3122353a9c
duchm22 <duchm.se22@gmail.com>
2022-05-18 17:04:45 +0700

    chore: attach data from api

--- src/modules/voting/components/voting-launch-section.vue
+++ src/modules/voting/components/voting-launch-section.vue
@@ -1,33 +1,39 @@
 <template>
-  <div>
-    <v-carousel height="auto" hide-delimiters>
-      <template v-slot:prev="{ on, attrs }">
-        <v-sheet
-          class="ml-n4 mt-n16 px-2 py-10 d-flex justify-center rounded-lg rounded-l-0 white--background"
-          v-bind="attrs"
-          v-on="on"
-        >
-          <v-icon color="black">mdi-chevron-left</v-icon>
-        </v-sheet>
-      </template>
-      <template v-slot:next="{ on, attrs }">
-        <v-sheet
-          v-bind="attrs"
-          v-on="on"
-          class="mr-n4 mt-n16 px-2 py-10 d-flex justify-center rounded-lg rounded-r-0 white--background"
-        >
-          <v-icon color="black">mdi-chevron-right</v-icon>
-        </v-sheet>
-      </template>
-      <v-carousel-item v-for="(prop, i) in data" :key="i">
-        <voting-launch-item :props="prop"></voting-launch-item>
-      </v-carousel-item>
-    </v-carousel>
-  </div>
+  <v-carousel height="auto" hide-delimiters>
+    <template v-slot:prev="{ on, attrs }">
+      <v-sheet
+        class="ml-n4 mt-n16 px-2 py-10 d-flex justify-center rounded-lg rounded-l-0 white--bg"
+        v-ripple
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon color="black">mdi-chevron-left</v-icon>
+      </v-sheet>
+    </template>
+    <template v-slot:next="{ on, attrs }">
+      <v-sheet
+        class="mr-n4 mt-n16 px-2 py-10 d-flex justify-center rounded-lg rounded-r-0 neutral100--bg"
+        v-ripple
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon color="black">mdi-chevron-right</v-icon>
+      </v-sheet>
+    </template>
+    <v-carousel-item v-for="(item, i) in vm.approvedList" :key="i">
+      <voting-launch-item
+        :projectName="item.projectName"
+        :data="item.data"
+        :type="item.type"
+        :endDate="item.endDate"
+      ></voting-launch-item>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Inject } from 'vue-property-decorator'
+import { VotingListViewModel } from '../viewmodels/voting-list-viewmodel'
 
 @Component({
   components: {
@@ -35,40 +41,12 @@ import { Component, Vue } from 'vue-property-decorator'
   },
 })
 export default class VotingLaunchSection extends Vue {
-  data = [
-    {
-      projectName: 'Hydro Wind Enery',
-      shortDescription: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      upvote: '90',
-      downvote: '90',
-      detailLink: '',
-      readmoreLink: '',
-      launchDate: 'JAN 21ST, 2022 AT 10:00 PM ONLY',
-    },
-    {
-      projectName: 'Hydro Wind Enery',
-      shortDescription: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      upvote: '90',
-      downvote: '90',
-      detailLink: '',
-      readmoreLink: '',
-      launchDate: 'JAN 21ST, 2022 AT 10:00 PM ONLY',
-    },
-    {
-      projectName: 'Hydro Wind Enery',
-      shortDescription: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      upvote: '90',
-      downvote: '90',
-      detailLink: '',
-      readmoreLink: '',
-      launchDate: 'JAN 21ST, 2022 AT 10:00 PM ONLY',
-    },
-  ]
+  @Inject() vm!: VotingListViewModel
 }
 </script>
 
 <style lang="scss" scoped>
-.white--background {
+.white--bg {
   background: white;
 }
 </style>
