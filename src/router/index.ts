d89248ba848442fa1d936b89a84c8cddd43ae821
duchm22 <duchm.se22@gmail.com>
2022-05-18 11:14:55 +0700

    feat: register mission form page

--- src/router/index.ts
+++ src/router/index.ts
@@ -72,6 +72,14 @@ const routes: Array<RouteConfig> = [
           title: 'Application',
         },
       },
+      {
+        path: 'new-mission',
+        component: () => import('@/modules/regist/pages/mission-form.vue'),
+        meta: {
+          auth: true,
+          title: 'Application',
+        },
+      },
     ],
   },
   {
