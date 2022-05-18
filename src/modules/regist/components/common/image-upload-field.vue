e20e1256375e05b43589cb62ec27b6ce1651cf7a
duchm22 <duchm.se22@gmail.com>
2022-05-18 10:23:48 +0700

    chore: update image uploader to check for null

--- src/modules/regist/components/common/image-upload-field.vue
+++ src/modules/regist/components/common/image-upload-field.vue
@@ -2,29 +2,35 @@
   <v-sheet
     outlined
     v-ripple
-    class="px-4 py-3 d-flex flex-column flex-md-row align-center cursor-pointer rounded transparent--bg"
+    class="pa-5 d-flex align-center cursor-pointer rounded transparent--bg"
     @click.stop="open"
     @change="handleChange"
   >
     <input type="file" ref="file-dialog" accept=".jpg,.png,.svg" hidden />
-    <img :src="source" width="42" height="42" class="rounded" />
-    <div class="d-flex flex-column flex-grow-1 ml-4">
+    <img v-if="image" :src="source" width="48" height="48" class="rounded" />
+    <img v-else :src="require('@/assets/icons/file-logo.svg')" width="42" height="42" class="rounded" />
+    <div class="d-flex flex-column flex-grow-1 ml-5">
       <span class="text-subtitle-1 font-weight-bold">{{ image ? image.name : 'Upload your image' }}</span>
-      <span class="text-subtitle-2 font-weight-regular">{{ imgSize ? imgSize : 'Allowed file types: png, jpg' }}</span>
+      <span class="text-subtitle-2 font-weight-regular">{{ imgSize || 'Allowed file types: png, jpg' }}</span>
     </div>
     <v-icon>mdi-upload</v-icon>
   </v-sheet>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Ref } from 'vue-property-decorator'
-import { get } from 'lodash'
+import { Component, Vue, Ref, Prop, Watch } from 'vue-property-decorator'
 import { convertBytes } from '@/helpers/file-helper'
+import { get, isNil } from 'lodash'
 @Component
 export default class ImageUploadField extends Vue {
+  @Prop({ default: null }) value!: File
   @Ref('file-dialog') dialog
 
-  image: any | null = null
+  image: File | null = null
+
+  created() {
+    console.log('mounted:::', this.value)
+  }
 
   open() {
     this.dialog.click()
@@ -35,12 +41,17 @@ export default class ImageUploadField extends Vue {
     this.$emit('change', this.image)
   }
 
+  @Watch('value', { immediate: true })
+  onPropChange(value: File) {
+    this.image = value
+  }
+
   get imgSize() {
-    return this.image && convertBytes(this.image!.size)
+    return !isNil(this.image) && convertBytes(this.image!.size)
   }
 
   get source() {
-    return this.image ? URL.createObjectURL(this.image) : require('@/assets/icons/file-logo.svg')
+    return !isNil(this.image) ? URL.createObjectURL(this.image) : ''
   }
 }
 </script>
