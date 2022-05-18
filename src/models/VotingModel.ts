4fba6a049cba1d8b8249f9f1279120bc265e095f
duchm22 <duchm.se22@gmail.com>
2022-05-18 17:02:33 +0700

    chore: add VotingModel

---
+++ src/models/VotingModel.ts
@@ -0,0 +1,21 @@
+export interface Metadata {
+  shortDescription: string
+  fields: string[]
+  projectCover: string
+  projectLogo: string
+  tokenContract: string
+  additionLink: string
+  chainID: string
+}
+
+export interface VotingPools {
+  projectName: string
+  rewardAddress: string
+  ownerAddress: string
+  data: Metadata
+  projectId: string
+  status: string
+  type: string
+  startDate: string
+  endDate: string
+}
