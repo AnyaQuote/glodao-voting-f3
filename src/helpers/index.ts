3acebd1d5daa302270bf37ae2d464539705604d2
duchm22 <duchm.se22@gmail.com>
2022-05-19 15:43:02 +0700

    chore: add unicode helper method

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,3 +6,10 @@ export const generateRandomString = (
     .map((x) => wishlist[x % wishlist.length])
     .join('')
 }
+
+export const unicodeMaker = (text: string) => {
+  return text
+    .toLowerCase()
+    .replace(/[^\w ]+/g, '')
+    .replace(/ +/g, '-')
+}
