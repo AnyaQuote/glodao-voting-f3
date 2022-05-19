4a29cd286b2e165b79472c99eb6aeefaefde5e48
duchm22 <duchm.se22@gmail.com>
2022-05-19 15:43:26 +0700

    feat: add fetch voting detail method

--- src/modules/voting/viewmodels/voting-detail-viewmodel.ts
+++ src/modules/voting/viewmodels/voting-detail-viewmodel.ts
@@ -1,9 +1,31 @@
+import { appProvider } from '@/app-providers'
+import { loadingController } from '@/components/global-loading/global-loading-controller'
 import { observable, action } from 'mobx'
+import { asyncAction } from 'mobx-utils'
 
 export class VotingDetailViewModel {
-  @observable show = false
+  @observable slug = ''
+  @observable voteDetail
 
-  @action.bound toggle() {
-    this.show = !this.show
+  constructor() {
+    //
+  }
+
+  @action getUnicode(value: string) {
+    this.slug = value
+  }
+
+  @asyncAction *fetchVoteDetail(codeName: string) {
+    try {
+      loadingController.increaseRequest()
+      const res = yield appProvider.api.voting.findOne({
+        unicode: this.slug,
+      })
+      this.voteDetail = res
+    } catch (error) {
+      appProvider.snackbar.commonError(error)
+    } finally {
+      loadingController.decreaseRequest()
+    }
   }
 }
