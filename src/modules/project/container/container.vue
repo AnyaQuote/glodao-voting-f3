5704d84b5c85dfb219560412d30211ee166a57e4
duchm22 <duchm.se22@gmail.com>
2022-05-20 19:34:41 +0700

    style: create banner for project list

--- src/modules/project/container/container.vue
+++ src/modules/project/container/container.vue
@@ -1,7 +1,14 @@
 <template>
-  <v-container>
-    <router-view></router-view>
-  </v-container>
+  <div>
+    <v-img class="blue mb-12" height="216" src="@/assets/images/project-banner-background.png">
+      <v-sheet class="transparent--bg px-4 text-h3 fill-height d-flex align-end mx-auto" max-width="1090">
+        <span class="mb-12 font-weight-bold white--text">Your project</span>
+      </v-sheet>
+    </v-img>
+    <v-container>
+      <router-view></router-view>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,4 +18,12 @@ import { Component, Vue } from 'vue-property-decorator'
 export default class Container extends Vue {}
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import '@/styles/breakpoint-custom.scss';
+.custom-left-margin {
+  margin-left: 20px;
+  @include breakpoint(desktop) {
+    transform: translate(40%);
+  }
+}
+</style>
