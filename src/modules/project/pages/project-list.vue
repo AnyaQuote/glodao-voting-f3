7dcab246fbe05d18c3eb8e85fdd62804ee43393e
duchm22 <duchm.se22@gmail.com>
2022-05-20 19:34:29 +0700

    chore: create UI for project list

--- src/modules/project/pages/project-list.vue
+++ src/modules/project/pages/project-list.vue
@@ -1,12 +1,58 @@
 <template>
-  <div></div>
+  <v-sheet class="rounded-lg neutral100--bg" outlined>
+    <v-sheet class="d-flex justify-space-between align-center text-h6 rounded-t-lg blue lighten-3">
+      <v-sheet height="58" class="d-flex align-stretch">
+        <v-sheet
+          width="240"
+          class="d-flex justify-center align-center font-weight-bold rounded-tl-lg"
+          :class="index === 1 ? 'blue white--text' : 'neutral10--text'"
+          @click="tab(1)"
+        >
+          Bounty project
+        </v-sheet>
+        <v-sheet
+          width="240"
+          class="d-flex justify-center align-center font-weight-bold"
+          :class="index === 2 ? 'blue white--text' : 'neutral10--text'"
+          @click="tab(2)"
+        >
+          Lauchpad project
+        </v-sheet>
+      </v-sheet>
+      <v-checkbox
+        dense
+        hide-details
+        label="Hide rejected project"
+        class="pa-0 ma-0 mr-8"
+        color="blue"
+        :input-value="vm.hideProjectFilter"
+        @change="vm.hideProject()"
+      ></v-checkbox>
+    </v-sheet>
+    <v-sheet class="padding-form">
+      <project-card v-for="(project, index) in vm.bountyProjects" :key="index" :project="project" class="mb-6" />
+    </v-sheet>
+  </v-sheet>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Provide } from 'vue-property-decorator'
+import { ProjectListViewModel } from '../viewmodels/project-list-viewmodel'
 
-@Component
-export default class extends Vue {}
+@Component({
+  components: {
+    'project-card': () => import('../components/project-card.vue'),
+  },
+})
+export default class ProjectListPage extends Vue {
+  @Provide() vm = new ProjectListViewModel()
+
+  index = 1
+
+  tab(index: number): void {
+    if (this.index !== index) this.index = index
+  }
+}
 </script>
 
 <style scoped></style>
