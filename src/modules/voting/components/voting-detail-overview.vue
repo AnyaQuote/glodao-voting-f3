57178c9fa1d49ba77ba2e144d26598f75db5521e
duchm22 <duchm.se22@gmail.com>
2022-05-20 16:32:04 +0700

    chore: update voting-detail-overview to receive props

--- src/modules/voting/components/voting-detail-overview.vue
+++ src/modules/voting/components/voting-detail-overview.vue
@@ -3,25 +3,24 @@
     <!-- ------------------------------------------------------------------------------------------------------------- -->
     <v-sheet class="banner">
       <div class="banner-top rounded-lg">
-        <v-img class="banner-img rounded-lg rounded-b-0" src="@/assets/images/voting-home--background.png" />
+        <v-img class="banner-img rounded-lg rounded-b-0" :src="vm.projectCover" />
         <voting-out-btn class="banner-link-btn" width="51" height="51" />
         <div class="banner-content rounded-lg py-10">
           <div class="text-h5 text-center font-weight-bold mb-10 white--text">Current result</div>
           <!-- CENTER CONTENT BOX START -->
           <v-sheet max-width="600" class="d-flex justify-center transparent white--text mx-16">
             <div class="d-flex">
-              <voting-progress-circle class="flex-shrink-0" value="90" color="green" />
+              <voting-progress-circle class="flex-shrink-0" :value="`${upvote}`" color="green" />
               <div class="d-flex flex-column justify-space-around flex-grow-1 ml-4">
                 <span class="text-h6">👍YES</span>
-                <span class="font-weight-bold font-14">1000 votes</span>
+                <span class="font-weight-bold font-14">{{ upvote }} votes</span>
               </div>
             </div>
-            <div class="mx-7"></div>
             <div class="d-flex">
-              <voting-progress-circle class="flex-shrink-0 mr-4" value="10" color="error" />
+              <voting-progress-circle class="flex-shrink-0 mr-4" :value="`${downvote}`" color="error" />
               <div class="d-flex flex-column justify-space-around flex-grow-1">
-                <span class="text-h6">👍NO</span>
-                <span class="font-weight-bold font-14">1000 votes</span>
+                <span class="text-h6">👎NO</span>
+                <span class="font-weight-bold font-14">{{ downvote }} votes</span>
               </div>
             </div>
           </v-sheet>
@@ -36,10 +35,10 @@
     <v-sheet class="pa-6 rounded-lg rounded-t-0">
       <div class="row">
         <div class="col-8">
-          <div class="text-h4 font-weight-bold">Hydro Wind Energy</div>
+          <div class="text-h4 font-weight-bold">{{ vm.projectName }}</div>
         </div>
         <div class="col-4">
-          <countdown to="2022-06-08T00:00:00.000Z" class="text-h5" />
+          <countdown :to="vm.endDate" class="text-h5" />
         </div>
         <div class="col-12">
           <div class="row">
@@ -57,36 +56,34 @@
           </div>
         </div>
         <div class="col-12">
-          <ul class="mb-n4">
+          <div>
+            {{ vm.shortDescription }}
+          </div>
+          <!-- <ul class="mb-n4">
             <li v-for="i in [1, 2, 3]" :key="i" class="mb-4">
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis
               enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Velit officia consequat duis enim
               velit mollit. Exercitation veniam consequat sunt nostrud amet.
             </li>
-          </ul>
+          </ul> -->
         </div>
 
         <div class="col-8 ma-n1">
-          <v-chip class="ma-1" color="orange lighten-4" text-color="orange" label>Bounty poject</v-chip>
-          <v-chip class="ma-1" label>Faming</v-chip>
-          <v-chip class="ma-1" label>NFT</v-chip>
-          <v-chip class="ma-1" label>Finance</v-chip>
+          <v-chip class="ma-1" :color="`${typeColor} lighten-4`" :text-color="typeColor" label>{{ vm.type }}</v-chip>
+          <v-chip class="ma-1 text-uppercase" label v-for="(field, i) in vm.fields" :key="i">{{ field }}</v-chip>
         </div>
 
         <div class="col-4 d-flex justify-end align-center">
-          <v-btn icon color="bluePrimary">
-            <v-icon small>fab fa-medium</v-icon>
+          <v-btn
+            v-for="(item, index) in vm.socialLinks"
+            :href="$_get(item, 'link')"
+            color="bluePrimary"
+            :key="index"
+            icon
+          >
+            <v-icon small>fab fa-{{ $_get(item, 'icon') }}</v-icon>
           </v-btn>
-          <v-btn icon color="bluePrimary">
-            <v-icon small>fab fa-twitter</v-icon>
-          </v-btn>
-          <v-btn icon color="bluePrimary">
-            <v-icon small>fab fa-twitter</v-icon>
-          </v-btn>
-          <v-btn icon color="bluePrimary">
-            <v-icon small>fab fa-telegram</v-icon>
-          </v-btn>
-          <v-btn icon color="bluePrimary">
+          <v-btn icon color="bluePrimary" :href="vm.website">
             <icon-git-book />
           </v-btn>
         </div>
@@ -100,7 +97,8 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Ref } from 'vue-property-decorator'
+import { Component, Vue, Ref, Inject } from 'vue-property-decorator'
+import { VotingDetailViewModel } from '../viewmodels/voting-detail-viewmodel'
 
 @Component({
   components: {
@@ -117,6 +115,11 @@ import { Component, Vue, Ref } from 'vue-property-decorator'
 export default class VotingDetailOverview extends Vue {
   @Ref('vote-prep-dialog') votePrepDialog
   @Ref('vote-confirm-dialog') voteConfirmDialog
+  @Inject() vm!: VotingDetailViewModel
+
+  upvote = Math.floor(Math.random() * 100) + 1
+  downvote = 100 - this.upvote
+  typeColor = this.vm.type === 'bounty' ? 'orange' : 'green'
 
   openDialog() {
     this.votePrepDialog.open()
