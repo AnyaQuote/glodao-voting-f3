30635a00147c0b5dde65a93f47f0fe1dc6a613f0
duchm22 <duchm.se22@gmail.com>
2022-05-21 18:27:24 +0700

    chore: update UI and props for project-detail component

--- src/modules/project/pages/project-detail.vue
+++ src/modules/project/pages/project-detail.vue
@@ -1,12 +1,101 @@
 <template>
-  <div></div>
+  <v-row>
+    <!-- ---------------------------- BREADCRUMBS --------------------------------------- -->
+    <v-col cols="12" class="mb-16">
+      <div class="mt-5 d-flex align-center font-weight-medium">
+        <span class="bluePrimary--text mr-5 cursor-pointer" @click="goToProjects">Your project</span>
+        <v-icon class="mr-5" size="22">mdi-chevron-right</v-icon>
+        <span class="neutral10--text">{{ vm.projectName }}</span>
+      </div>
+    </v-col>
+
+    <!-- ---------------------------- PROJECT DETAIL HEADER ---------------------------- -->
+    <v-col cols="12" class="mb-12">
+      <v-sheet class="row rounded-lg blue lighten-5 mx-1 pa-6 align-center" elevation="3">
+        <div class="col-md-6 col-12 d-flex align-center">
+          <v-img max-height="76" max-width="76" :src="vm.projectLogo"></v-img>
+          <div class="d-flex flex-column ml-4">
+            <div class="text-h5 font-weight-bold mb-1">Dragon Gaming</div>
+            <div class="neutral10--text d-flex align-center">
+              <div class="text-h6 font-weight-bold mr-1">$SB</div>
+              <v-chip class="text-capitalize" :class="statusColor">{{ vm.status }}</v-chip>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-12 d-flex">
+          <div class="flex-grow-1">
+            <div class="neutral0--text">Total misstion</div>
+            <div class="text-h6 font-weight-bold">100</div>
+          </div>
+          <div class="flex-grow-1">
+            <div class="neutral0--text">Total reward</div>
+            <div class="text-h6 font-weight-bold">1000 BUSD</div>
+          </div>
+          <v-btn
+            width="162"
+            height="48"
+            class="linear-blue--bg neutral100--text text-subtitle-1 text-none"
+            depressed
+            @click="goToNewMission"
+          >
+            Create misstion
+          </v-btn>
+        </div>
+      </v-sheet>
+    </v-col>
+
+    <!-- ---------------------------- MISSION CARDS ---------------------------- -->
+    <v-col v-for="(mission, index) in vm.missions" :key="index" cols="4">
+      <mission-card
+        :image="mission.image"
+        :name="mission.name"
+        :totalReward="mission.totalReward"
+        :priorityReward="mission.priorityReward"
+        :start="mission.start"
+        :end="mission.end"
+        :index="index"
+      />
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Inject } from 'vue-property-decorator'
+import { ProjectDetailViewModel } from '../viewmodels/project-detail-viewmodel'
+import { get } from 'lodash-es'
+import { RoutePaths } from '@/router'
+import { Observer } from 'mobx-vue'
 
-@Component
-export default class extends Vue {}
+@Observer
+@Component({
+  components: {
+    'mission-card': () => import('../components/mission-card.vue'),
+  },
+})
+export default class ProjectDetailPage extends Vue {
+  @Inject('project-detail-vm') vm!: ProjectDetailViewModel
+
+  goToProjects() {
+    this.$router.push(RoutePaths.project_list)
+  }
+
+  goToNewMission() {
+    this.$router.push(RoutePaths.new_mission)
+  }
+
+  mounted() {
+    console.log(get(this.$route, 'params.code'))
+    this.vm.getUnicode(get(this.$route, 'params.code'))
+  }
+
+  beforeDestroy() {
+    this.vm.dispose()
+  }
+
+  get statusColor() {
+    return this.vm.status === 'approved' ? 'green ' : this.vm.status === 'rejected' ? 'red' : 'neutral100'
+  }
+}
 </script>
 
 <style scoped></style>
