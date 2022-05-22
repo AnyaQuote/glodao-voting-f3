2bd9b4b6025cd8070530a97d0e9f3cc40ea19658
duchm22 <duchm22.se@gmail.com>
2022-05-22 16:38:25 +0700

    chore: create project banner

---
+++ src/modules/project/components/banner.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-img class="blue mb-12" height="216" src="@/assets/images/project/banner.png">
+    <v-sheet class="transparent--bg px-4 pb-12 text-h3 fill-height d-flex align-end mx-auto" max-width="1090">
+      <span class="font-weight-bold white--text flex-grow-1">Your project</span>
+      <v-btn class="linear-blue--bg text-none px-6 flex-shrink-0" height="55" depressed @click="goToNewProject">
+        <span class="white--text font-weight-bold font-18">New application</span>
+      </v-btn>
+    </v-sheet>
+  </v-img>
+</template>
+
+<script lang="ts">
+import { RoutePaths } from '@/router'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ProjectBanner extends Vue {
+  goToNewProject() {
+    this.$router.push(RoutePaths.new_application)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/breakpoint-custom.scss';
+.custom-left-margin {
+  margin-left: 20px;
+  @include breakpoint(desktop) {
+    transform: translate(40%);
+  }
+}
+</style>
