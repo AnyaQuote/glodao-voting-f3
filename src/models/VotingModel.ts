c6087b011c9e75ac409b70306e1d8e9a6f8b9094
duchm22 <duchm22.se@gmail.com>
2022-05-22 11:44:56 +0700

    fix: add undefined for each interface property

--- src/models/VotingModel.ts
+++ src/models/VotingModel.ts
@@ -1,23 +1,32 @@
 export interface Metadata {
-  shortDescription: string
-  fields: string[]
-  projectCover: string
-  projectLogo: string
+  shortDescription?: string
+  fields?: string[]
+  projectCover?: string
+  projectLogo?: string
   tokenContract?: string
   additionLink?: string
   chainID?: string
-  socialLinks: any
+  socialLinks?: any
 }
 
 export interface VotingPools {
-  projectName: string
-  rewardAddress: string
-  ownerAddress: string
-  data: Metadata
-  projectId: string
-  status: string
-  type: string
-  startDate: string
-  endDate: string
-  unicode: string
+  projectName?: string
+  rewardAddress?: string
+  ownerAddress?: string
+  data?: Metadata
+  projectId?: string
+  status?: string
+  type?: string
+  startDate?: string
+  endDate?: string
+  unicode?: string
+}
+
+export interface Mission {
+  name?: string
+  image?: string
+  totalReward?: string
+  priorityReward?: string
+  start?: string
+  end?: string
 }
