83989ea73506620277444f46f3451a541e448387
duchm22 <duchm22.se@gmail.com>
2022-05-23 02:26:39 +0700

    style: update voting detail for mobile view

--- src/modules/voting/components/common/reward-card.vue
+++ src/modules/voting/components/common/reward-card.vue
@@ -1,11 +1,23 @@
 <template>
-  <v-sheet class="rounded-lg pa-6 neutral10--text font-weight-medium" elevation="3">
+  <v-sheet v-if="$vuetify.breakpoint.mdAndUp" class="rounded-lg pa-6 neutral10--text font-weight-medium" elevation="3">
     <div>Total reward</div>
     <div class="d-flex align-center">
-      <span class="text-h6 mr-2">{{ props.rewardAmount | formatNumber }} {{ props.tokenName }}</span>
-      <icon-crypto-currency />
+      <span class="text-h6 mr-2"
+        >{{ props.rewardAmount | formatNumber }} {{ props.tokenName }}
+        <icon-crypto-currency />
+      </span>
     </div>
     <div>{{ props.rewardExchange | usdCustom }}</div>
+  </v-sheet>
+  <v-sheet v-else class="rounded-lg pa-6 neutral10--text font-weight-medium" elevation="3">
+    <div class="d-flex align-center justify-space-between">
+      <div>Total reward</div>
+      <div class="d-flex align-center">
+        <span class="text-h6 mr-2">{{ props.rewardAmount | formatNumber }} {{ props.tokenName }}</span>
+        <icon-crypto-currency />
+      </div>
+    </div>
+    <div class="text-end">{{ props.rewardExchange | usdCustom }}</div>
   </v-sheet>
 </template>
 
--- src/modules/voting/components/voting-detail-overview.vue
+++ src/modules/voting/components/voting-detail-overview.vue
@@ -1,32 +1,52 @@
 <template>
   <v-sheet class="neutral100-bg rounded-lg" elevation="3" outlined>
     <!-- ------------------------------------------------------------------------------------------------------------- -->
-    <v-sheet class="banner">
-      <div class="banner-top rounded-lg">
-        <v-img class="banner-img rounded-lg rounded-b-0" :src="vm.projectCover" />
-        <voting-out-btn class="banner-link-btn" width="51" height="51" />
-        <div class="banner-content rounded-lg py-10">
-          <div class="text-h5 text-center font-weight-bold mb-10 white--text">Current result</div>
-          <!-- CENTER CONTENT BOX START -->
-          <v-sheet max-width="600" class="d-flex justify-center transparent white--text mx-16">
-            <div class="d-flex">
-              <voting-progress-circle class="flex-shrink-0" :value="`${upvote}`" color="green" />
-              <div class="d-flex flex-column justify-space-around flex-grow-1 ml-4">
-                <span class="text-h6">👍YES</span>
-                <span class="font-weight-bold font-14">{{ upvote }} votes</span>
+    <v-sheet>
+      <v-img v-if="$vuetify.breakpoint.smAndUp" class="rounded-t-lg" :src="vm.projectCover">
+        <div class="d-flex flex-column align-center fill-height">
+          <voting-out-btn class="align-self-end mt-4 mr-4" width="51" height="51" />
+          <div class="fill-height d-flex align-center pb-16">
+            <div class="rounded-lg py-10 blur--bg">
+              <div class="text-h5 text-center font-weight-bold mb-10 white--text">Current result</div>
+              <!-- CENTER CONTENT BOX START -->
+              <div class="d-flex justify-center transparent white--text mx-16">
+                <div class="d-flex mr-10">
+                  <voting-progress-circle class="flex-shrink-0" :value="`${upvote}`" color="green" />
+                  <div class="d-flex flex-column justify-space-around flex-grow-1 ml-4">
+                    <span class="text-h6">👍YES</span>
+                    <span class="font-weight-bold font-14">{{ upvote }} votes</span>
+                  </div>
+                </div>
+                <div class="d-flex">
+                  <voting-progress-circle class="flex-shrink-0 mr-4" :value="`${downvote}`" color="error" />
+                  <div class="d-flex flex-column justify-space-around flex-grow-1">
+                    <span class="text-h6">👎NO</span>
+                    <span class="font-weight-bold font-14">{{ downvote }} votes</span>
+                  </div>
+                </div>
               </div>
+
+              <!-- CENTER CONTENT BOX END -->
             </div>
-            <div class="d-flex">
-              <voting-progress-circle class="flex-shrink-0 mr-4" :value="`${downvote}`" color="error" />
-              <div class="d-flex flex-column justify-space-around flex-grow-1">
-                <span class="text-h6">👎NO</span>
-                <span class="font-weight-bold font-14">{{ downvote }} votes</span>
-              </div>
-            </div>
-          </v-sheet>
-          <!-- CENTER CONTENT BOX END -->
+          </div>
         </div>
-      </div>
+      </v-img>
+      <v-sheet v-else>
+        <v-img max-height="244" class="rounded-t-lg" :src="vm.projectCover" />
+        <div class="pa-4 d-flex flex-column">
+          <div class="font-18 font-weight-bold mb-2">Current result</div>
+          <v-sheet class="rounded-lg pa-4 mb-2 green lighten-4 d-flex align-baseline" outlined>
+            <span class="font-18 font-weight-bold mr-8">{{ upvote }}%</span>
+            <span class="text-subtitle-2 neutral10--text spacer">1000 upvotes</span>
+            <v-chip label color="green lighten-2" class="white--text rounded-lg px-6">👍 YES votes</v-chip>
+          </v-sheet>
+          <v-sheet class="rounded-lg pa-4 red lighten-4 d-flex align-baseline" outlined>
+            <span class="font-18 font-weight-bold mr-8">{{ downvote }}%</span>
+            <span class="text-subtitle-2 neutral10--text spacer"> 1000 downvotes</span>
+            <v-chip label color="red lighten-1" class="white--text rounded-lg px-6">👎 NO votes</v-chip>
+          </v-sheet>
+        </div>
+      </v-sheet>
       <voting-detail-button @buttonClick="openDialog" />
     </v-sheet>
     <!-- BANNER SECTION END -->
@@ -34,7 +54,7 @@
     <!-- CONTENT SECTION START -->
     <v-sheet class="pa-6 rounded-lg rounded-t-0">
       <div class="row">
-        <div class="col-8">
+        <div class="col-md-8 col-12">
           <div class="text-h4 font-weight-bold">{{ vm.projectName }}</div>
         </div>
         <div class="col-4">
@@ -42,7 +62,7 @@
         </div>
         <div class="col-12">
           <div class="row">
-            <div class="col-3" v-for="i in [1, 2, 3]" :key="i">
+            <div class="col-md-3 col-12" v-for="i in [1, 2, 3]" :key="i">
               <reward-card class="reward-card" />
             </div>
             <!-- TOOLTIP SECTION START -->
@@ -68,12 +88,12 @@
           </ul> -->
         </div>
 
-        <div class="col-8 ma-n1">
+        <div class="col-md-8 col-12 ma-n1">
           <v-chip class="ma-1" :color="`${typeColor} lighten-4`" :text-color="typeColor" label>{{ vm.type }}</v-chip>
           <v-chip class="ma-1 text-uppercase" label v-for="(field, i) in vm.fields" :key="i">{{ field }}</v-chip>
         </div>
 
-        <div class="col-4 d-flex justify-end align-center">
+        <div class="col-md-4 col-12 d-flex justify-md-end justify-start align-center">
           <v-btn
             v-for="(item, index) in vm.socialLinks"
             :href="$_get(item, 'link')"
@@ -128,34 +148,8 @@ export default class VotingDetailOverview extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.font-14 {
-  font-size: em(14);
-}
-.banner {
-  position: relative;
-  .banner-top {
-    position: relative;
-    .banner-img {
-      position: relative;
-      background: var(--v-neutral10-base);
-    }
-    .banner-link-btn {
-      position: absolute;
-      bottom: em(23);
-      left: em(43);
-    }
-  }
-  .banner-content {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: rgba(0, 0, 0, 0.5);
-    backdrop-filter: blur(10px);
-  }
-}
-
-.reward-card {
-  @extend .font-14;
+.blur--bg {
+  backdrop-filter: blur(8px);
+  background-color: rgba(0, 0, 0, 0.5) !important;
 }
 </style>
