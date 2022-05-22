655c7a3791d6c5ebb88ba74197a79bebe0a7125d
duchm22 <duchm22.se@gmail.com>
2022-05-22 12:40:00 +0700

    chore: update prop

--- src/modules/project/components/project-card.vue
+++ src/modules/project/components/project-card.vue
@@ -4,30 +4,34 @@
       <!------------------------------------------ CARD RIGHT -------------------------------------------------->
       <v-col cols="6" style="border-right: thin solid var(--v-neutral20-base)">
         <v-sheet height="124" class="pa-6 d-flex blue lighten-3 rounded-tl-lg">
-          <v-img class="d-flex justify-center align-center mr-4" max-width="76" max-height="78" :src="projectLogo">
-            <v-skeleton-loader v-show="!projectLogo" type="image" />
+          <v-img
+            class="d-flex justify-center align-center mr-4"
+            max-width="76"
+            max-height="78"
+            :src="$_get(pool, 'data.projectLogo')"
+          >
           </v-img>
 
           <v-spacer class="d-flex flex-column justify-center">
-            <div class="text-h5 font-weight-bold">{{ projectName }}</div>
+            <div class="text-h5 font-weight-bold">{{ $_get(pool, 'projectName') }}</div>
             <div class="text-h6 neutral10--text">$SB</div>
           </v-spacer>
         </v-sheet>
 
         <v-divider></v-divider>
 
-        <v-sheet class="pa-6"> {{ data.shortDescription }} </v-sheet>
+        <v-sheet class="pa-6"> {{ $_get(pool, 'data.shortDescription') }} </v-sheet>
 
         <v-divider></v-divider>
 
         <v-sheet class="pa-6">
           <v-sheet class="d-flex">
             <v-sheet class="mr-2">Voting start: </v-sheet>
-            <v-sheet class="font-weight-bold">{{ startDate | ddmmyyyyhhmma }}</v-sheet>
+            <v-sheet class="font-weight-bold">{{ $_get(pool, 'startDate') | ddmmyyyyhhmma }}</v-sheet>
           </v-sheet>
           <v-sheet class="d-flex">
             <v-sheet class="mr-2">Voting start: </v-sheet>
-            <v-sheet class="font-weight-bold">{{ endDate | ddmmyyyyhhmma }}</v-sheet>
+            <v-sheet class="font-weight-bold">{{ $_get(pool, 'endDate') | ddmmyyyyhhmma }}</v-sheet>
           </v-sheet>
         </v-sheet>
       </v-col>
@@ -92,7 +96,7 @@
 </template>
 
 <script lang="ts">
-import { Metadata } from '@/models/VotingModel'
+import { VotingPools } from '@/models/VotingModel'
 import { RoutePaths } from '@/router'
 import { get } from 'lodash'
 import { Observer } from 'mobx-vue'
@@ -101,21 +105,14 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 @Observer
 @Component
 export default class ProjectCard extends Vue {
-  @Prop({ required: true }) projectName!: string
-  @Prop({ required: true }) startDate!: string
-  @Prop({ required: true }) endDate!: string
-  @Prop({ required: true }) status!: string
-  @Prop({ required: true }) type!: string
-  @Prop({ required: true }) unicode!: string
-  @Prop({ required: true }) data!: Metadata
+  @Prop({ required: true }) pool!: VotingPools
 
-  projectLogo = get(this.data, 'projectLogo', '')
   upvoteCount = Math.floor(Math.random() * 1000) + 500
   downvoteCount = Math.floor(Math.random() * 500)
   totalVoteCount = this.upvoteCount + this.downvoteCount
 
   goToDetail() {
-    this.$router.push(RoutePaths.project_detail + this.unicode)
+    this.$router.push(RoutePaths.project_detail + this.pool.unicodeName)
   }
 
   get upvote() {
@@ -123,11 +120,15 @@ export default class ProjectCard extends Vue {
   }
 
   get statusClass() {
-    return this.status === 'approved' ? 'green lighten-1' : 'redSenamatic'
+    return this.pool.status === 'approved' ? 'green lighten-1' : 'redSenamatic'
   }
 
   get statusReport() {
-    return this.status === 'approved' ? 'Your project is approved' : 'Your project is rejected'
+    return this.pool.status === 'approved' ? 'Your project is approved' : 'Your project is rejected'
+  }
+
+  get projectLogo() {
+    return get(this.pool, 'data.projectLogo', '')
   }
 }
 </script>
