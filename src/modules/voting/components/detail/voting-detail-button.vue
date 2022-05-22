1618afc8f3e1d4a6553a36e530e58f3dda1e32b4
duchm22 <duchm22.se@gmail.com>
2022-05-23 02:43:58 +0700

    fix: UI responsive

--- src/modules/voting/components/detail/voting-detail-button.vue
+++ src/modules/voting/components/detail/voting-detail-button.vue
@@ -7,16 +7,16 @@
     <span class="white--text text-h5">Please connect wallet to vote for project</span>
   </v-sheet>
   <!-- I DONT KNOW WHY BUT DO NOT REMOVE mx-0 FROM row -->
-  <v-sheet v-else class="d-flex flex-column pa-6 fill-height blue lighten-5 overflow-hidden">
-    <div class="mb-4">
-      <div class="text-subtitle-1">Your connected wallet:</div>
-      <div class="text-subtitle-2 d-flex align-center">
-        <icon-chain class="mr-1" />
-        <span>{{ wallet.account }}</span>
+  <v-sheet v-else class="pa-6 fill-height blue lighten-5 overflow-hidden">
+    <div v-if="$vuetify.breakpoint.mdAndUp" class="d-flex flex-row">
+      <div class="mr-4">
+        <div class="text-subtitle-1">Your connected wallet:</div>
+        <div class="text-subtitle-2 d-flex align-center">
+          <icon-chain class="mr-1" />
+          <span>{{ wallet.account }}</span>
+        </div>
       </div>
-    </div>
-    <div class="d-flex mb-4">
-      <div class="d-flex flex-column mr-6">
+      <div class="d-flex flex-column mr-4">
         <span class="text-subtitle-1">Staking</span>
         <span class="text-subtitle-2">1000 GLD</span>
       </div>
@@ -24,10 +24,40 @@
         <span class="mr-1">TRIBE</span>
         <v-icon>mdi-fingerprint</v-icon>
       </v-sheet>
+      <v-spacer />
+      <v-btn color="bluePrimary" height="48" class="white--text rounded-lg" depressed @click="$emit('buttonClick')">
+        <span class="px-5">Vote</span>
+      </v-btn>
     </div>
-    <v-btn color="bluePrimary" height="48" class="white--text rounded-lg" depressed @click="$emit('buttonClick')">
-      <span class="px-5">Cast your vote</span>
-    </v-btn>
+    <div v-else class="d-flex flex-column">
+      <div class="mb-4">
+        <div class="text-subtitle-1">Your connected wallet:</div>
+        <div class="text-subtitle-2 d-flex align-center">
+          <icon-chain class="mr-1" />
+          <span>{{ wallet.account }}</span>
+        </div>
+      </div>
+      <div class="d-flex mb-4">
+        <div class="d-flex flex-column mr-6">
+          <span class="text-subtitle-1">Staking</span>
+          <span class="text-subtitle-2">1000 GLD</span>
+        </div>
+        <v-sheet class="rounded pa-3 d-flex align-center" outlined>
+          <span class="mr-1">TRIBE</span>
+          <v-icon>mdi-fingerprint</v-icon>
+        </v-sheet>
+      </div>
+      <v-btn
+        color="bluePrimary"
+        height="48"
+        block
+        class="white--text rounded-lg"
+        depressed
+        @click="$emit('buttonClick')"
+      >
+        <span class="px-5">Cast your vote</span>
+      </v-btn>
+    </div>
   </v-sheet>
 </template>
 
