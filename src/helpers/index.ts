ed78a78dffe47909ff8e8aaf8d38c9844ff2dcc6
duchm22 <duchm22.se@gmail.com>
2022-05-22 17:11:33 +0700

    chore: remove makeUnicode method helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,10 +6,3 @@ export const generateRandomString = (
     .map((x) => wishlist[x % wishlist.length])
     .join('')
 }
-
-export const unicodeMaker = (text: string) => {
-  return text
-    .toLowerCase()
-    .replace(/[^\w ]+/g, '')
-    .replace(/ +/g, '-')
-}
