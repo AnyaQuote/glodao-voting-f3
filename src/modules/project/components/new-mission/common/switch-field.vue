87dfe5c90bc1abf9bd6557b84a45625668b15133
duchm22 <duchm22.se@gmail.com>
2022-05-26 07:24:23 +0700

    chore: add icon slot, emit switch value on changed

--- src/modules/project/components/new-mission/common/switch-field.vue
+++ src/modules/project/components/new-mission/common/switch-field.vue
@@ -1,17 +1,27 @@
 <template>
   <v-sheet class="d-flex flex-column transparent--bg rounded-lg" outlined>
     <div class="d-flex align-center pa-5">
-      <img width="48" height="48" contain :src="require('@/assets/icons/twitter.svg')" />
+      <slot name="icon">
+        <img v-if="type === 'twitter'" width="48" height="48" contain :src="require('@/assets/icons/twitter.svg')" />
+        <img
+          v-else-if="type === 'telegram'"
+          width="48"
+          height="48"
+          contain
+          :src="require('@/assets/icons/telegram.svg')"
+        />
+        <img v-else width="48" height="48" contain :src="require('@/assets/icons/learn-to-earn.svg')" />
+      </slot>
       <div class="flex-grow-1 d-flex ml-5">
         <div class="font-weight-bold text-subtitle-1 mr-4">{{ title }}</div>
         <span class="bullet text-subtitle-1 font-weight-regular">{{ subtitle }}</span>
       </div>
-      <v-switch color="app-blue" v-model="show" />
+      <v-switch color="app-blue" :value="show" @change="onChange" />
     </div>
     <div v-if="show && hasSlot">
-      <v-divider class="ma-5" />
+      <v-divider class="my-2 mx-6" />
       <div class="px-5 py-6">
-        <slot :required="show"></slot>
+        <slot></slot>
       </div>
     </div>
   </v-sheet>
@@ -19,15 +29,21 @@
 
 <script lang="ts">
 import { Observer } from 'mobx-vue'
-import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
+import { Component, Vue, Prop } from 'vue-property-decorator'
 
 @Observer
 @Component
 export default class SwitchField extends Vue {
+  @Prop({ required: true }) type!: string
   @Prop({ default: '' }) title!: string
   @Prop({ default: '' }) subtitle!: string
 
   show = false
+
+  onChange(value: boolean) {
+    this.show = value
+    this.$emit('change', value)
+  }
 
   get hasSlot() {
     return this.$slots.default
