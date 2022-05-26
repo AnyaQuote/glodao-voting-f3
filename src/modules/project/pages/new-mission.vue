bb14259b34fc9141fb70177f83c419807f652c29
duchm22 <duchm.se22@gmail.com>
2022-05-26 16:55:06 +0700

    style: add skeleton loading for form

--- src/modules/project/pages/new-mission.vue
+++ src/modules/project/pages/new-mission.vue
@@ -1,13 +1,32 @@
 <template>
   <v-container>
     <v-row justify="center" class="mb-100">
+      <!-- ---------------- BREADCRUMBS ---------------- -->
       <v-col cols="12">
         <div class="d-flex align-center justify-start" @click="goBack">
           <v-icon>mdi-arrow-left</v-icon>
           <span class="text-h6 bluePrimary--text cursor-pointer ml-7">Back to project</span>
         </div>
       </v-col>
-      <v-col cols="10">
+
+      <!-- ---------------- SKELETON LOADER ---------------- -->
+      <v-col v-if="vm.pageLoading" cols="10">
+        <v-sheet class="neutral100--bg rounded-lg pb-7" outlined
+          ><div class="blue lighten-4 py-6 text-center rounded-lg rounded-b-0">
+            <span class="text-h5 font-weight-bold text-uppercase">Create mission</span>
+          </div>
+          <v-divider />
+          <v-skeleton-loader class="mt-7" type="list-item-two-line" />
+          <v-skeleton-loader type="list-item" />
+          <v-skeleton-loader type="image" class="mx-4" height="80" />
+          <v-skeleton-loader class="mt-7" type="list-item" />
+          <v-skeleton-loader type="image" class="mx-4" height="100" />
+          <v-skeleton-loader type="list-item-two-line" />
+        </v-sheet>
+      </v-col>
+
+      <!-- ---------------- FORM CONTENT ---------------- -->
+      <v-col v-else cols="10">
         <v-sheet class="neutral100--bg rounded-lg" outlined>
           <div class="blue lighten-4 py-6 text-center rounded-lg rounded-b-0">
             <span class="text-h5 font-weight-bold text-uppercase">Create mission</span>
@@ -20,7 +39,7 @@
             <mission-setting />
             <v-btn
               class="linear-blue--bg white--text text-none mt-7"
-              :loading="vm.loading"
+              :loading="vm.btnLoading"
               @click="submit"
               height="40"
               depressed
@@ -39,7 +58,7 @@
 import { Observer } from 'mobx-vue'
 import { Component, Vue, Provide, Ref } from 'vue-property-decorator'
 import { NewMissionViewModel } from '../viewmodels/new-mission-viewmodel'
-
+import { get } from 'lodash-es'
 @Observer
 @Component({
   components: {
@@ -48,7 +67,7 @@ import { NewMissionViewModel } from '../viewmodels/new-mission-viewmodel'
   },
 })
 export default class MissionForm extends Vue {
-  @Provide() vm = new NewMissionViewModel()
+  @Provide() vm = new NewMissionViewModel(get(this.$route, 'params.code'))
   @Ref('form') form
   goBack() {
     this.$router.go(-1)
