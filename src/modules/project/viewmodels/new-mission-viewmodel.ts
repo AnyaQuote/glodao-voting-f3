d355f54d997aa2c6e4a72b4a71ecd80702975e55
duchm22 <duchm.se22@gmail.com>
2022-05-26 10:47:06 +0700

    fix: add image upload for

--- src/modules/project/viewmodels/new-mission-viewmodel.ts
+++ src/modules/project/viewmodels/new-mission-viewmodel.ts
@@ -1,5 +1,5 @@
 import { appProvider } from '@/app-providers'
-import { getJSONFromFile } from '@/helpers/file-helper'
+import { getApiFileUrl, getJSONFromFile } from '@/helpers/file-helper'
 import { Data } from '@/models/MissionModel'
 import { Quiz, LearnToEarn } from '@/models/QuizModel'
 import { Mission } from '@/models/MissionModel'
@@ -96,6 +96,10 @@ export class NewMissionViewModel {
   @observable learnToEarn = learnToEarnDefault
   @observable loading = false
 
+  constructor() {
+    //
+  }
+
   @action.bound changeMissionInfo(property: string, value: string) {
     set(this.missionInfo, property, value)
   }
@@ -126,8 +130,15 @@ export class NewMissionViewModel {
   }
 
   @asyncAction *getQuizId() {
-    const { quizFile, learningFile, name, description } = this.learnToEarn.setting!
-    let quizJSON, answerJSON, learningInformation
+    const { quizFile, learningFile, imageCover, name, description } = this.learnToEarn.setting!
+    let quizJSON, answerJSON, learningInformation, coverUrl
+    if (imageCover) {
+      const media = new FormData()
+      media.append('files', imageCover)
+      coverUrl = getApiFileUrl(yield appProvider.api.uploadFile(media))
+    } else {
+      // get project Cover for learn to earn cover
+    }
     if (quizFile) {
       const textData = yield quizFile.text()
       ;[quizJSON, answerJSON] = getJSONFromFile(textData)
@@ -140,7 +151,7 @@ export class NewMissionViewModel {
       description,
       learningInformation,
       metadata: {
-        coverImage: '',
+        coverImage: coverUrl,
         tags: ['nft'],
       },
       data: quizJSON,
@@ -153,20 +164,22 @@ export class NewMissionViewModel {
   @asyncAction *getMissionSetting() {
     const missionSetting = {}
     if (this.joinTelegram.enabled) {
-      set(missionSetting, 'telegram', [...get(missionSetting, 'telegram', []), this.joinTelegram.setting])
+      // set(missionSetting, 'telegram', [...get(missionSetting, 'telegram', []), this.joinTelegram.setting])
+      set(missionSetting, 'telegram', [{ ...this.joinTelegram.setting }])
     }
     if (this.followTwitter.enabled) {
-      set(missionSetting, 'twitter', [...get(missionSetting, 'twitter', []), this.followTwitter.setting])
+      set(missionSetting, 'twitter', [...get(missionSetting, 'twitter', []), { ...this.followTwitter.setting }])
     }
     if (this.quoteTweet.enabled) {
-      set(missionSetting, 'twitter', [...get(missionSetting, 'twitter', []), this.quoteTweet.setting])
+      set(missionSetting, 'twitter', [...get(missionSetting, 'twitter', []), { ...this.quoteTweet.setting }])
     }
     if (this.commentTweet.enabled) {
-      set(missionSetting, 'twitter', [...get(missionSetting, 'twitter', []), this.commentTweet.setting])
+      set(missionSetting, 'twitter', [...get(missionSetting, 'twitter', []), { ...this.commentTweet.setting }])
     }
     if (this.learnToEarn.enabled) {
       const quiz = yield this.getQuizId()
-      set(missionSetting, 'quiz', [...get(missionSetting, 'quiz', []), { type: 'quiz', quizId: quiz.id }])
+      // set(missionSetting, 'quiz', [...get(missionSetting, 'quiz', []), { type: 'quiz', quizId: quiz.id }])
+      set(missionSetting, 'quiz', [{ type: 'quiz', quizId: quiz.id }])
     }
     return missionSetting
   }
@@ -175,37 +188,38 @@ export class NewMissionViewModel {
     try {
       this.loading = true
       const data = yield this.getMissionSetting()
-      const mission: Mission = {
-        name: 'Hận Toản',
-        type: 'bounty',
-        status: 'draft',
-        chainId: 'BSC',
-        tokenBasePrice: '1.2',
-        rewardAmount: '1000',
-        startTime: moment().toISOString(),
-        endTime: moment().toISOString(),
-        data,
-        metadata: {
-          shortDescription:
-            'Decentralized autonomous community of users, investors, and partners called GloDAO. This is an open ecosystem that combines Web3.0 Social Networking, DAO Organization, and Advertising. ',
-          decimals: '18',
-          projectLogo: 'https://diversity-api.contracts.dev/uploads/Tubbly_logo_3x_e3e569ffda.png',
-          tokenLogo: 'https://api.glodao.io/uploads/BUSD_Logo_2cc6a78969.svg',
-          coverImage: 'https://diversity-api.contracts.dev/uploads/Facebook_cover_1_b3e93b5f13.png',
-          caption:
-            "GloDAO establishes itself as a decentralized autonomous community of users, investors, and partners. This provides an open ecosystem and a fusion of Web3.0 Social Networking - DAO organization - Advertising, all in one place. <br/>GloDAO was born with a purpose: to support and promote the full potential of the projects and optimize its user performance. <br/>GloDAO is on its way to form a publicly open, transparent, and completely decentralized environment. The vast range of users that this can touch includes community members, projects, and KOL partners who can generate even more value and wealth when participating in GloDAO's ecosystem.",
-          rewardToken: 'BUSD',
-          socialLinks: {
-            twitter: 'https://twitter.com/GloDAO_Official',
-            telegram: 'https://t.me/GloDAO_Channel',
-            facebook: 'https://www.facebook.com/GloDao-105742212066225',
-          },
-          website: 'https://glodao.io/',
-        },
-      }
-      console.log('final:::', mission)
-      const fuck = yield appProvider.api.tasks.create(mission)
-      console.log('res:::', fuck)
+      console.log('mission.data:::', data)
+      // const mission: Mission = {
+      //   name: 'Hận Toản',
+      //   type: 'bounty',
+      //   status: 'draft',
+      //   chainId: 'BSC',
+      //   tokenBasePrice: '1.2',
+      //   rewardAmount: '1000',
+      //   startTime: moment().toISOString(),
+      //   endTime: moment().toISOString(),
+      //   data,
+      //   metadata: {
+      //     shortDescription:
+      //       'Decentralized autonomous community of users, investors, and partners called GloDAO. This is an open ecosystem that combines Web3.0 Social Networking, DAO Organization, and Advertising. ',
+      //     decimals: '18',
+      //     projectLogo: 'https://diversity-api.contracts.dev/uploads/Tubbly_logo_3x_e3e569ffda.png',
+      //     tokenLogo: 'https://api.glodao.io/uploads/BUSD_Logo_2cc6a78969.svg',
+      //     coverImage: 'https://diversity-api.contracts.dev/uploads/Facebook_cover_1_b3e93b5f13.png',
+      //     caption:
+      //       "GloDAO establishes itself as a decentralized autonomous community of users, investors, and partners. This provides an open ecosystem and a fusion of Web3.0 Social Networking - DAO organization - Advertising, all in one place. <br/>GloDAO was born with a purpose: to support and promote the full potential of the projects and optimize its user performance. <br/>GloDAO is on its way to form a publicly open, transparent, and completely decentralized environment. The vast range of users that this can touch includes community members, projects, and KOL partners who can generate even more value and wealth when participating in GloDAO's ecosystem.",
+      //     rewardToken: 'BUSD',
+      //     socialLinks: {
+      //       twitter: 'https://twitter.com/GloDAO_Official',
+      //       telegram: 'https://t.me/GloDAO_Channel',
+      //       facebook: 'https://www.facebook.com/GloDao-105742212066225',
+      //     },
+      //     website: 'https://glodao.io/',
+      //   },
+      // }
+      // console.log('final:::', mission)
+      // const fuck = yield appProvider.api.tasks.create(mission)
+      // console.log('res:::', fuck)
       appProvider.snackbar.addSuccess()
     } catch (error) {
       appProvider.snackbar.commonError(error)
