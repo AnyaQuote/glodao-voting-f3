52e53c3c2ee373f99dd176b22c663d1b6ad8e539
duchm22 <duchm22.se@gmail.com>
2022-05-26 07:23:46 +0700

    chore: add form validation, form state

--- src/modules/project/components/new-mission/mission-setting.vue
+++ src/modules/project/components/new-mission/mission-setting.vue
@@ -1,87 +1,138 @@
 <template>
   <div class="mt-7">
     <div class="title font-weight-bold bluePrimary--text">Mission setting</div>
-    <!-- Setting one -->
+    <!-- -------------------------------------- Join telegram -------------------------------------- -->
     <switch-field
       class="mt-4"
+      type="telegram"
       title="Telegram task"
-      :value="settingOneValue"
-      @change="vm.updateMissionSetting('twitter', $event)"
       subtitle="Join telegram group"
-    ></switch-field>
+      @change="vm.changeJoinTelegramSetting('enabled', $event)"
+    >
+      <app-text-field
+        :rules="[$rules.required]"
+        :value="$_get(vm.joinTelegram, 'setting.link')"
+        @change="vm.changeJoinTelegramSetting('setting.link', $event)"
+        placeholder="Enter telegram group link"
+        append-icon="mdi-link"
+      />
+    </switch-field>
 
-    <!-- Setting two -->
+    <!-- -------------------------------------- Follow twitter -------------------------------------- -->
     <switch-field
       class="mt-4"
+      type="twitter"
       title="Twitter task"
-      :value="settingTwoValue"
-      @change="vm.updateMissionSetting('twitter', $event)"
       subtitle="Follow project twitter"
-    />
-
-    <!-- Setting three -->
-    <switch-field class="mt-4" title="Twitter task" value="" subtitle="Quote a tweet">
-      <!-- <template v-slot="{ required }"> -->
-      <v-form ref="1">
-        <app-text-field placeholder="Enter your hastag" append-icon="fa-solid fa-hashtag" />
-      </v-form>
-      <!-- </template> -->
+      @change="vm.changeFollowTwitterSetting('enabled', $event)"
+    >
+      <app-text-field
+        :rules="[$rules.required]"
+        :value="$_get(vm.followTwitter, 'setting.link')"
+        @change="vm.changeFollowTwitterSetting('setting.link', $event)"
+        placeholder="Enter your twitter projects link"
+        append-icon="mdi-link"
+      />
     </switch-field>
 
-    <!-- Setting four -->
-    <switch-field class="mt-4" title="Twitter task" subtitle="Like and reply a post">
-      <!-- <template v-slot="{ required }"> -->
-      <v-form ref="2">
-        <app-text-field placeholder="Enter your tweet link" append-icon="mdi-link" />
-      </v-form>
-      <!-- </template> -->
+    <!-- -------------------------------------- Quote tweet -------------------------------------- -->
+    <switch-field
+      class="mt-4"
+      type="twitter"
+      title="Twitter task"
+      subtitle="Quote a tweet"
+      @change="vm.changeQuoteTweetSetting('enabled', $event)"
+    >
+      <div class="font-18 font-weight-bold">Twitter link</div>
+      <app-text-field
+        class="mt-2"
+        :rules="[$rules.required]"
+        :value="$_get(vm.quoteTweet, 'setting.link')"
+        @change="vm.changeQuoteTweetSetting('setting.link', $event)"
+        placeholder="Enter twitter link"
+        append-icon="mdi-link"
+      />
+      <div class="font-18 font-weight-bold mt-6">Twitter hastag</div>
+      <app-text-field
+        class="mt-2"
+        :rules="[$rules.required]"
+        :value="$_get(vm.quoteTweet, 'setting.hashtag')"
+        @change="vm.changeQuoteTweetSetting('setting.hashtag', $event)"
+        placeholder="Enter your hastag"
+        append-icon="fa-solid fa-hashtag"
+      />
     </switch-field>
 
-    <!-- Setting learn to earn quiz -->
-    <switch-field class="mt-4" title="Learn to earn" subtitle="Learn document and answer question">
-      <!-- <template v-slot="{ required }"> -->
-      <v-form ref="3" class="d-flex flex-column">
-        <div class="font-18 font-weight-bold">Task name</div>
-        <app-text-field
-          :rules="[$rules.required]"
-          @change="vm.changeLearnToEarnInfo('name', $event)"
-          class="mt-2"
-          placeholder="Enter task name"
-        />
-        <div class="font-18 font-weight-bold mt-6">Short description</div>
-        <app-textarea
-          :rules="[$rules.required]"
-          @change="vm.changeLearnToEarnInfo('shortDescription', $event)"
-          class="mt-2"
-          placeholder="Enter short description that describes the mission"
-        />
-        <div class="font-18 font-weight-bold mt-6">Quiz cover</div>
-        <image-upload-field @change="vm.changeLearnToEarnInfo('imageCover', $event)" class="mt-2" />
-        <div class="font-18 mt-6 d-flex">
-          <span class="font-weight-bold">Quiz file</span>
-          <v-spacer />
-          <span class="app-blue--text">Download the quiz template</span>
-        </div>
-        <file-selector :rules="[$rules.required]" @change="vm.changeLearnToEarnInfo('quizFile', $event)" class="mt-2" />
-        <div class="d-flex mt-6">
-          <span class="font-weight-bold">Document</span>
-          <v-spacer />
-          <span class="app-blue--text">Download the quiz template</span>
-        </div>
-        <file-selector
-          :rules="[$rules.required]"
-          @change="vm.changeLearnToEarnInfo('learningFile', $event)"
-          class="mt-2"
-        />
-      </v-form>
-      <!-- </template> -->
+    <!-- -------------------------------------- Like and repost tweet -------------------------------------- -->
+    <switch-field
+      class="mt-4"
+      type="twitter"
+      title="Twitter task"
+      subtitle="Like and reply a post"
+      @change="vm.changeCommentTweetSetting('enabled', $event)"
+    >
+      <app-text-field
+        :rules="[$rules.required]"
+        :value="$_get(vm.quoteTweet, 'setting.link')"
+        @change="vm.changeCommentTweetSetting('setting.link', $event)"
+        placeholder="Enter your tweet link"
+        append-icon="mdi-link"
+      />
+    </switch-field>
+
+    <!-- -------------------------------------- Setting learn to earn quiz ------------------------- -->
+    <switch-field
+      class="mt-4"
+      type="learn-to-earn"
+      title="Learn to earn"
+      subtitle="Learn document and answer question"
+      @change="vm.changeLearnToEarnInfo('enabled', $event)"
+    >
+      <div class="font-18 font-weight-bold">Task name</div>
+      <app-text-field
+        :rules="[$rules.required]"
+        :value="$_get(vm.learnToEarn, 'setting.name')"
+        @change="vm.changeLearnToEarnInfo('setting.name', $event)"
+        class="mt-2"
+        placeholder="Enter task name"
+      />
+      <div class="font-18 font-weight-bold mt-6">Description</div>
+      <app-textarea
+        :rules="[$rules.required]"
+        :value="$_get(vm.learnToEarn, 'setting.description')"
+        @change="vm.changeLearnToEarnInfo('setting.description', $event)"
+        class="mt-2"
+        placeholder="Enter short description that describes the mission"
+      />
+      <div class="font-18 font-weight-bold mt-6">Quiz cover</div>
+      <image-upload-field @change="vm.changeLearnToEarnInfo('setting.imageCover', $event)" class="mt-2" />
+      <div class="font-18 mt-6 d-flex">
+        <span class="font-weight-bold">Quiz file</span>
+        <v-spacer />
+        <span class="app-blue--text">Download the quiz template</span>
+      </div>
+      <file-selector
+        :rules="[$rules.required]"
+        @change="vm.changeLearnToEarnInfo('setting.quizFile', $event)"
+        class="mt-2"
+      />
+      <div class="d-flex mt-6">
+        <span class="font-weight-bold">Document</span>
+        <v-spacer />
+        <span class="app-blue--text">Download the quiz template</span>
+      </div>
+      <file-selector
+        :rules="[$rules.required]"
+        @change="vm.changeLearnToEarnInfo('setting.learningFile', $event)"
+        class="mt-2"
+      />
     </switch-field>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Inject, Ref } from 'vue-property-decorator'
-import { NewMissionViewModel, settingOneValue, settingTwoValue } from '../../viewmodels/new-mission-viewmodel'
+import { NewMissionViewModel } from '../../viewmodels/new-mission-viewmodel'
 import { Observer } from 'mobx-vue'
 
 @Observer
@@ -94,16 +145,6 @@ import { Observer } from 'mobx-vue'
 })
 export default class MissionSettingForm extends Vue {
   @Inject() vm!: NewMissionViewModel
-  @Ref('1') form1
-  @Ref('2') form2
-  @Ref('3') form3
-
-  settingOneValue = settingOneValue
-  settingTwoValue = settingTwoValue
-
-  mounted() {
-    this.vm.injectForm([this.form1, this.form2, this.form3])
-  }
 }
 </script>
 
