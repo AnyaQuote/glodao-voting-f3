615b8a64018f292723f332a0577f126d8b2e5530
duchm22 <duchm.se22@gmail.com>
2022-05-27 19:23:07 +0700

    fix: format number for mission reward and project detail headers tweaks

--- src/modules/project/components/project-detail-components/project-others/header.vue
+++ src/modules/project/components/project-detail-components/project-others/header.vue
@@ -4,7 +4,7 @@
     <v-row no-gutters dense class="app-blue lighten-1 rounded-t-lg pa-6">
       <v-col cols="6" class="d-flex align-stretch">
         <v-sheet height="76" width="76" class="white pa-1 rounded-lg d-flex justify-center mr-6">
-          <img :src="$_get(vm.poolStore, 'projectLogo', '')" height="100%" />
+          <img :src="$_get(vm.poolStore, 'projectLogo')" height="100%" />
         </v-sheet>
         <div class="d-flex flex-column justify-center">
           <div class="text-h5 font-weight-bold neutral0--text mb-1">{{ $_get(vm.poolStore, 'projectName') }}</div>
@@ -27,7 +27,7 @@
           <v-col cols="4">
             <div class="text-subtitle-1 neutral0--text">Reward/mission</div>
             <div class="text-h6 neutral0--text font-weight-bold">
-              {{ $_get(vm.poolStore, 'rewardPerMission') | formatNumber(2, 2) }} BUSD
+              {{ $_get(vm.poolStore, 'rewardPerMission') | formatNumber(2, 2) }} {{ $_get(vm.poolStore, 'tokenName') }}
             </div>
           </v-col>
         </v-row>
@@ -38,17 +38,23 @@
     <v-row no-gutters dense class="pa-6">
       <v-col cols="6">
         <div class="text-subtitle-1 neutral0--text">Campaign time</div>
-        <div class="text-h6 neutral0--text font-weight-bold">Jan 21st, 10:00 pm</div>
+        <div class="text-h6 neutral0--text font-weight-bold">
+          {{ $_get(vm.poolStore, 'startDate') | ddmmyyyyhhmma }} {{ $_get(vm.poolStore, 'endDate') | ddmmyyyyhhmma }}
+        </div>
       </v-col>
       <v-col cols="6">
         <v-row dense no-gutters align="center">
           <v-col cols="4">
             <div class="text-subtitle-1 neutral0--text">Created</div>
-            <div class="text-h6 neutral0--text font-weight-bold">{{ $_get(vm, 'missions.length') }} missions</div>
+            <div class="text-h6 neutral0--text font-weight-bold">
+              {{ $_get(vm, 'missions.length') }} {{ $_get(vm, 'missions.length') > 0 ? 'missions' : 'mission' }}
+            </div>
           </v-col>
           <v-col cols="4">
             <div class="text-subtitle-1 neutral0--text">Remaining</div>
-            <div class="text-h6 neutral0--text font-weight-bold">{{ remainingMission }} missions</div>
+            <div class="text-h6 neutral0--text font-weight-bold">
+              {{ remainingMission }} {{ remainingMission > 0 ? 'missions' : 'mission' }}
+            </div>
           </v-col>
           <v-col cols="4">
             <v-btn
@@ -58,7 +64,7 @@
               style="letter-spacing: 0"
               @click="goToNewMission"
               height="48"
-              elevation="0"
+              depressed
               block
             >
               Create mission
