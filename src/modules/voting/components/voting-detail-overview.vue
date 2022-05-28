09bb0eac485e3381bca10b035cc896438edb1a68
duchm22 <duchm22.se@gmail.com>
2022-05-28 23:30:18 +0700

    style: update UI for voting detail overview

--- src/modules/voting/components/voting-detail-overview.vue
+++ src/modules/voting/components/voting-detail-overview.vue
@@ -1,63 +1,57 @@
 <template>
   <v-sheet class="neutral100-bg rounded-lg" elevation="3" v-if="vm.poolStore" outlined>
-    <!-- ------------------------------------------------------------------------------------------------------------- -->
-    <v-sheet>
-      <v-img
-        v-if="$vuetify.breakpoint.smAndUp"
-        max-height="614"
-        aspect-ratio="1"
-        class="rounded-t-lg"
-        :src="$_get(vm.poolStore, 'projectCover')"
-      >
-        <div class="d-flex flex-column align-center fill-height">
-          <div class="fill-height d-flex align-center pb-16">
-            <div class="rounded-lg py-10 blur--bg">
-              <div class="d-flex justify-center transparent white--text mx-16">
-                <div>
-                  <div class="d-flex align-center justify-space-between">
-                    <v-chip label color="app-green lighten-1" class="text-subtitle-2 white--text mb-2">👍 YES</v-chip>
-                    <span class="font-weight-bold font-14">{{ $_get(vm.poolStore, 'votedPercent', 0) }} %</span>
-                  </div>
-                  <v-progress-linear
-                    style="width: 400px"
-                    :value="+$_get(vm.poolStore, 'votedPercent', 0)"
-                    color="success"
-                    height="25"
-                  ></v-progress-linear>
-                </div>
+    <!-- ------------------------------------- BANNER ------------------------------------------------------------------------ -->
+    <v-img
+      v-if="$vuetify.breakpoint.smAndUp"
+      max-height="614"
+      aspect-ratio="1"
+      class="rounded-t-lg"
+      :src="$_get(vm.poolStore, 'projectCover')"
+    >
+      <div class="d-flex flex-column align-center fill-height">
+        <div class="fill-height d-flex align-center pb-16">
+          <div class="rounded-lg py-10 blur--bg">
+            <v-sheet width="400" class="d-flex flex-column transparent white--text mx-16">
+              <div class="font-weight-bold mb-2 text-center">Current result</div>
+              <div class="d-flex align-center justify-space-between">
+                <v-chip label color="app-green lighten-1" class="text-subtitle-2 white--text mb-2">👍 YES</v-chip>
+                <span class="font-weight-bold font-14">{{ $_get(vm.poolStore, 'votedPercent', 0) }} %</span>
               </div>
-            </div>
+              <v-progress-linear
+                :value="+$_get(vm.poolStore, 'votedPercent', 0)"
+                color="success"
+                height="25"
+              ></v-progress-linear>
+            </v-sheet>
           </div>
-          <voting-out-btn class="align-self-start mb-8 ml-8" />
         </div>
-      </v-img>
-      <v-sheet v-else>
-        <v-img max-height="244" class="rounded-t-lg" :src="vm.poolStore.projectCover" />
-        <div class="pa-4 d-flex flex-column">
-          <div class="font-18 font-weight-bold mb-2">Current result</div>
-          <v-sheet class="rounded-lg pa-4 mb-2 green lighten-4 d-flex align-baseline" outlined>
-            <span class="font-18 font-weight-bold mr-8">---%</span>
-            <span class="text-subtitle-2 neutral10--text spacer">--- upvotes</span>
-            <v-chip label color="green lighten-2" class="white--text rounded-lg px-6">👍 YES votes</v-chip>
-          </v-sheet>
-          <!-- <v-sheet class="rounded-lg pa-4 red lighten-4 d-flex align-baseline" outlined>
-            <span class="font-18 font-weight-bold mr-8">{{ downvote }}%</span>
-            <span class="text-subtitle-2 neutral10--text spacer"> 1000 downvotes</span>
-            <v-chip label color="red lighten-1" class="white--text rounded-lg px-6">👎 NO votes</v-chip>
-          </v-sheet> -->
-        </div>
-      </v-sheet>
+        <voting-out-btn class="align-self-start mb-8 ml-8" />
+      </div>
+    </v-img>
+    <v-sheet v-else>
+      <v-img max-height="244" class="rounded-t-lg" :src="vm.poolStore.projectCover" />
+      <div class="pa-4 d-flex flex-column">
+        <div class="font-18 font-weight-bold mb-2">Current result</div>
+        <v-sheet class="rounded-lg pa-4 mb-2 green lighten-4 d-flex align-baseline black--text" outlined>
+          <span class="font-18 font-weight-bold mr-8">---%</span>
+          <span class="text-subtitle-2 spacer">--- upvotes</span>
+          <v-chip label color="green lighten-2" class="white--text rounded-lg px-6">👍 YES votes</v-chip>
+        </v-sheet>
+      </div>
     </v-sheet>
+    <!-- ------------------------------------- VOTE BUTTON  ------------------------------------------------------------------ -->
     <voting-detail-button @buttonClick="openConfirmDialog" />
-    <!-- BANNER SECTION END -->
+    <!-- ------------------------------------- OVERVIEW ---------------------------------------------------------------------- -->
     <v-sheet class="pa-6 rounded-lg rounded-t-0">
       <div class="row">
-        <div class="col-md-8 col-12">
-          <div class="text-h4 font-weight-bold">{{ $_get(vm.poolStore, 'projectName') }}</div>
+        <!-- ==== PROJECT NAME ==== -->
+        <div class="col-md-8 col-12 text-h4 font-weight-bold">
+          {{ $_get(vm.poolStore, 'projectName') }}
         </div>
-        <div class="col-4">
+        <!-- ==== COUNT DOWN AND STATUS ==== -->
+        <div class="col-md-4 col-12">
           <countdown :to="$_get(vm.poolStore, 'endDate')" v-if="$_get(vm.poolStore, 'onVoting')" class="text-h5" />
-          <v-card
+          <v-sheet
             v-else
             height="40"
             class="d-flex justify-center align-center rounded font-weight-600 text-subtitle-1"
@@ -65,32 +59,42 @@
             outlined
           >
             {{ statusReport.text }}
-          </v-card>
+          </v-sheet>
         </div>
-
+        <!-- ==== CARDS ==== -->
         <div class="col-12">
-          <div class="row">
-            <v-sheet class="rounded-lg pa-6 neutral10--text font-weight-medium mr-4" elevation="3">
-              <div>Total reward</div>
-              <div class="d-flex align-center">
-                <span class="text-h6 font-weight-bold"
-                  >{{ $_get(vm.poolStore, 'amount') | formatNumber(2, 0) }} {{ $_get(vm.poolStore, 'tokenName') }}
-                </span>
-              </div>
+          <div class="row ma-0">
+            <!-- Total reward card -->
+            <v-sheet
+              class="col-12 col-md-3 mb-2 mb-md-0 mr-md-4 mr-0 rounded-lg pa-6 neutral10--text d-flex flex-row flex-md-column align-md-start align-center justify-start justify-space-between"
+              elevation="3"
+              outlined
+            >
+              <div class="font-weight-medium">Total reward</div>
+              <span class="text-h6 font-weight-bold"
+                >{{ $_get(vm.poolStore, 'amount') | formatNumber(2, 0) }} {{ $_get(vm.poolStore, 'tokenName') }}
+              </span>
             </v-sheet>
-            <v-sheet class="rounded-lg pa-6 neutral10--text font-weight-medium mr-4" elevation="3">
-              <div>Total missions</div>
-              <div class="d-flex align-center">
-                <span class="text-h6 font-weight-bold">{{ vm.poolStore.totalMission | formatNumber(0) }} </span>
-              </div>
+            <!-- Total mission card -->
+            <v-sheet
+              class="col-12 col-md-3 mb-2 mb-md-0 mr-md-4 mr-0 rounded-lg pa-6 neutral10--text mr-4 d-flex flex-row flex-md-column align-md-start align-center justify-start justify-space-between"
+              elevation="3"
+              outlined
+            >
+              <div class="font-weight-medium">Total missions</div>
+              <span class="text-h6 font-weight-bold">{{ $_get(vm.poolStore, 'totalMission') | formatNumber(0) }} </span>
             </v-sheet>
-            <v-sheet class="rounded-lg pa-6 neutral10--text font-weight-medium mr-4" elevation="3">
-              <div>Mission reward</div>
-              <div class="d-flex align-center">
-                <span class="text-h6 font-weight-bold"
-                  >{{ vm.poolStore.rewardPerMission | formatNumber }} {{ vm.poolStore.tokenName }}</span
-                >
-              </div>
+            <!-- Mission reward card -->
+            <v-sheet
+              class="col-12 col-md-3 mb-2 mb-md-0 mr-0 mr-md-4 rounded-lg pa-6 neutral10--text d-flex flex-row flex-md-column align-md-start align-center justify-start justify-space-between"
+              elevation="3"
+              outlined
+            >
+              <span class="font-weight-medium">Mission reward</span>
+              <span class="text-h6 font-weight-bold"
+                >{{ $_get(vm.poolStore, 'rewardPerMission') | formatNumber }}
+                {{ $_get(vm.poolStore, 'tokenName') }}</span
+              >
             </v-sheet>
             <v-tooltip top>
               <template v-slot:activator="{ on, attrs }">
@@ -100,44 +104,39 @@
             </v-tooltip>
           </div>
         </div>
+        <!-- ==== DESCRIPTION ==== -->
         <div class="col-12">
           <ul>
             <li class="neutral-10--text">{{ vm.poolStore.shortDescription }}</li>
           </ul>
-          <!-- <ul class="mb-n4">
-            <li v-for="i in [1, 2, 3]" :key="i" class="mb-4">
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis
-              enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Velit officia consequat duis enim
-              velit mollit. Exercitation veniam consequat sunt nostrud amet.
-            </li>
-          </ul> -->
         </div>
-
+        <!-- ==== LABElS ==== -->
         <div class="col-md-8 col-12 ma-n1">
-          <v-chip class="ma-1" :color="`${typeColor} lighten-4`" :text-color="typeColor" label>{{
+          <v-chip class="ma-1 text-uppercase" :color="`${typeColor} lighten-5`" :text-color="typeColor" label>{{
             vm.poolStore.type
           }}</v-chip>
-          <v-chip class="ma-1 text-uppercase" label v-for="(field, i) in vm.poolStore.fields" :key="i">{{
+          <v-chip class="ma-1 text-uppercase neutral-20" label v-for="(field, i) in vm.poolStore.fields" :key="i">{{
             field
           }}</v-chip>
         </div>
-
+        <!-- ==== SOCIAL LINKS ==== -->
         <div class="col-md-4 col-12 d-flex justify-md-end justify-start align-center">
           <v-btn
             v-for="(item, index) in vm.socialLinks"
             :href="$_get(item, 'link')"
-            color="bluePrimary"
+            color="blue-diversity"
             :key="index"
             icon
           >
             <v-icon small>fab fa-{{ $_get(item, 'icon') }}</v-icon>
           </v-btn>
-          <v-btn icon color="bluePrimary" :href="vm.poolStore.website">
+          <v-btn icon color="blue-diversity" :href="$_get(vm.poolStore, 'website')">
             <icon-git-book />
           </v-btn>
         </div>
       </div>
     </v-sheet>
+    <!-- ------------------------------------------------------------------------------------------------------------------- -->
     <!-- <vote-prep-dialog ref="vote-prep-dialog" @openConfirmDialog="openConfirmDialog" /> -->
     <vote-confirm-dialog ref="vote-confirm-dialog" :voteResult="voteResult" />
   </v-sheet>
