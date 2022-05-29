053518954179d8a82007c83a5be4db87af025864
duchm22 <duchm22.se@gmail.com>
2022-05-29 22:01:28 +0700

    chore: import app-social-fields to use

--- src/modules/regist/components/regist-bounty/project-info.vue
+++ src/modules/regist/components/regist-bounty/project-info.vue
@@ -47,85 +47,17 @@
         outlined
       ></v-autocomplete>
 
-      <div class="font-18 font-weight-bold">Website and social link</div>
-      <app-text-field
-        class="thin-border"
-        :value="$_get(vm.projectInfo, 'socialLinks.website')"
-        @input="vm.changeProjectInfo('socialLinks.website', $event)"
-        :outlined="false"
-        append-icon="mdi-link"
-      >
-        <template #prepend>
-          <div class="w-130">
-            <v-icon class="ml-3">mdi-web</v-icon>
-            <span class="neutral10--text text-subtitle-2 font-weight-600 ml-3">Website</span>
-          </div>
-          <v-divider vertical></v-divider>
-        </template>
-      </app-text-field>
+      <!-- ===== SOCIAL LINKS FIELDS ===== -->
+      <div class="d-flex flex-column flex-md-row align-md-center justify-space-between">
+        <span class="font-18 font-weight-bold"> Website and social link</span>
+        <i class="text-subtitle-2 font-weight-regular">*Select a link for each types</i>
+      </div>
+      <app-socials-field
+        :value="$_get(vm.projectInfo, 'socialLinks')"
+        @change="vm.changeProjectInfo('socialLinks', $event)"
+        class="mt-3"
+      />
 
-      <app-text-field
-        class="mt-3 thin-border"
-        :outlined="false"
-        :value="$_get(vm.projectInfo, 'socialLinks.medium')"
-        @input="vm.changeProjectInfo('socialLinks.medium', $event)"
-        append-icon="mdi-link"
-      >
-        <template #prepend>
-          <div class="w-130">
-            <v-icon class="ml-3">fab fa-medium</v-icon>
-            <span class="neutral10--text text-subtitle-2 font-weight-600 ml-3">Medium</span>
-          </div>
-          <v-divider vertical></v-divider>
-        </template>
-      </app-text-field>
-
-      <app-text-field
-        class="mt-3 thin-border"
-        :outlined="false"
-        :value="$_get(vm.projectInfo, 'socialLinks.twitter')"
-        @input="vm.changeProjectInfo('socialLinks.twitter', $event)"
-        append-icon="mdi-link"
-      >
-        <template #prepend>
-          <div class="d-flex align-center w-130">
-            <v-avatar size="24" class="ml-3">
-              <v-img src="@/assets/icons/twitter.svg" />
-            </v-avatar>
-            <!-- <icon-wrapper class="ml-3" src="../../../../assets/icons/twitter.svg" /> -->
-            <span class="neutral10--text text-subtitle-2 font-weight-600 ml-3">Twitter</span>
-          </div>
-          <v-divider vertical></v-divider>
-        </template>
-      </app-text-field>
-
-      <app-text-field
-        class="mt-3 thin-border"
-        :outlined="false"
-        :value="$_get(vm.projectInfo, 'socialLinks.telegram')"
-        @input="vm.changeProjectInfo('socialLinks.telegram', $event)"
-        append-icon="mdi-link"
-      >
-        <template #prepend>
-          <div class="d-flex align-center w-130">
-            <v-icon color="blue" class="ml-3">fab fa-telegram</v-icon>
-            <span class="neutral10--text text-subtitle-2 font-weight-600 ml-3">Telegram</span>
-          </div>
-          <v-divider vertical></v-divider>
-        </template>
-      </app-text-field>
-
-      <v-sheet
-        class="neutral100--bg d-flex justify-start align-center text-none pa-3 mt-3 rounded"
-        depressed
-        width="100%"
-        height="fit-content"
-        outlined
-        v-ripple
-      >
-        <v-icon>mdi-plus-circle</v-icon>
-        <div class="neutral10--text text-subtitle-2 font-weight-600 line-height-1 ml-3">Add more link</div>
-      </v-sheet>
       <v-btn
         class="white--text font-weight-bold text-none mt-6"
         :class="valid && 'linear-blue--bg'"
@@ -148,6 +80,7 @@ import { BountyApplyViewModel } from '../../viewmodels/bounty-apply-viewmodel'
 @Component({
   components: {
     'image-upload-field': () => import('@/components/image-upload-field.vue'),
+    'app-socials-field': () => import('@/components/app-socials-field.vue'),
   },
 })
 export default class ProjectInfo extends Vue {
