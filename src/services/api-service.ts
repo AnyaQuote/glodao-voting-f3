73c4db09cda98a956c136c4fcd83bdc3491dd168
duchm22 <duchm.se22@gmail.com>
2022-05-30 11:21:06 +0700

    chore: turn off jwt for api that does not require it

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -186,9 +186,9 @@ export class ApiService {
   axios = Axios.create({ baseURL: process.env.VUE_APP_API_STRAPI_ENDPOINT })
 
   users = new ApiHandlerJWT<any>(this.axios, 'users', { find: false })
-  tasks = new ApiHandlerJWT<any>(this.axios, 'tasks')
+  tasks = new ApiHandlerJWT<any>(this.axios, 'tasks', { find: false, count: false })
   voting = new ApiHandlerJWT<any>(this.axios, 'voting-pools', { find: false, count: false, findOne: false })
-  quizzes = new ApiHandlerJWT<any>(this.axios, 'quizzes')
+  quizzes = new ApiHandlerJWT<any>(this.axios, 'quizzes', { find: false, count: false })
 
   constructor() {
     this.setupAuthInjection()
