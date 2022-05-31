253c1d3402e9ec1c073af31dcdcf324dc49400d6
Anh Tu <n.anhtu0101@gmail.com>
2022-05-31 10:39:50 +0700

    lint code

--- src/modules/project/pages/project-list.vue
+++ src/modules/project/pages/project-list.vue
@@ -2,7 +2,7 @@
   <div>
     <project-list-header />
 
-    <div class="spacing mx-auto">
+    <div class="spacing px-sm-4 mx-auto">
       <div v-if="vm.loading" class="px-3 py-3">
         <div class="d-flex mb-3">
           <v-skeleton-loader class="mr-3" type="chip" />
@@ -75,109 +75,6 @@
         </div>
       </div>
     </div>
-
-    <!-- <v-container>
-      <v-row no-gutters dense> -->
-    <!-- ----------------------- LOADING SKELETON --------------------------- -->
-    <!-- <div v-if="vm.loading" class="row">
-          <div class="col-12 d-flex">
-            <v-skeleton-loader class="mr-3" type="chip" />
-            <v-skeleton-loader type="chip" />
-            <v-spacer />
-            <v-skeleton-loader type="chip" />
-          </div>
-          <v-skeleton-loader class="col-12" type="image" />
-        </div> -->
-    <!-- ----------------------- EMPTY PROJECT --------------------------- -->
-    <!-- <v-col cols="12" v-else-if="!vm.loading && !vm.votingPools.length">
-          <div class="row mt-100">
-            <div class="col-12 col-md-7">
-              <div class="text-h3 mb-9 font-weight-bold">Create the first application</div>
-              <div class="neutral10--text text-h6 mb-9">
-                Your interested projects should create a profile using this website, create and submit an application.
-                GloDAO investors will review and vote for your project on Voting. If your project has enough votes, it
-                will be launched on Bounty Huter or Launchpad.
-              </div>
-              <div v-if="!walletStore.account">
-                <ConnectWallet />
-              </div>
-              <v-btn
-                v-else
-                class="linear-blue--bg white--text text-none"
-                height="fit-content"
-                depressed
-                @click="goToNewProject"
-              >
-                <span class="ma-4 font-18 font-weight-bold">New application</span>
-              </v-btn>
-            </div>
-            <v-spacer />
-            <div class="col-12 col-md-4 order-first order-md-1">
-              <v-img height="324" width="324" src="@/assets/images/project/image-1.png" />
-            </div>
-          </div>
-        </v-col> -->
-    <!-- ------------------------ HAS PROJECTS ---------------------------- -->
-    <!-- <v-col cols="12" v-else>
-          <v-sheet class="rounded-lg neutral-100">
-            <v-sheet class="d-flex justify-space-between align-center text-h6 rounded-t-lg blue-2">
-              <v-sheet height="58" class="d-flex align-stretch rounded-tl-lg"> -->
-    <!--- ==== TAB FILTER ==== -->
-    <!-- <v-sheet
-                  width="240"
-                  class="d-flex align-center justify-center cursor-pointer rounded-tl-lg"
-                  :class="activeClass('bounty')"
-                  @click="vm.changeFilterdType('bounty')"
-                  :light="true"
-                >
-                  <span class="font-weight-bold">Bounty project</span>
-                </v-sheet>
-                <v-sheet
-                  width="240"
-                  class="d-flex justify-center align-center cursor-pointer"
-                  :class="activeClass('launchpad')"
-                  @click="vm.changeFilterdType('launchpad')"
-                >
-                  <span class="font-weight-bold">Lauchpad project</span>
-                </v-sheet>
-              </v-sheet> -->
-    <!-- ==== CHECKBOX HIDE REJECTED ==== -->
-    <!-- <v-checkbox
-                v-model="vm.filterRejected"
-                dense
-                hide-details
-                label="Hide rejected project"
-                class="pa-0 ma-0 mr-8 neutral-0--text"
-                color="app-blue"
-              ></v-checkbox>
-            </v-sheet> -->
-    <!-- ==== CONTENT DISPLAY ==== -->
-    <!-- EMPTY DATA -->
-    <!-- <v-scale-transition leave-absolute v-if="!vm.filteredStatusProjects.length">
-              <v-sheet class="d-flex flex-column align-center justify-center" height="300">
-                <img width="80" height="80" src="@/assets/icons/project/empty-icon.svg" class="mb-4" />
-                <div class="font-weight-bold text-h6">You have no {{ vm.filterType }} projects</div>
-                <div class="text-subtitle-1 neutral10--text mb-4">Ready to apply for a new project?</div>
-                <v-btn class="linear-blue--bg text-none" height="40" depressed @click="goToNewProject">
-                  <span class="white--text font-weight-bold">New {{ vm.filterType }} application</span>
-                </v-btn>
-              </v-sheet>
-            </v-scale-transition> -->
-    <!-- HAS DATA -->
-    <!-- <v-scale-transition leave-absolute v-else>
-              <div class="padding-form">
-                <project-list-card
-                  v-for="(pool, index) in vm.filteredStatusProjects"
-                  :key="index"
-                  :pool="pool"
-                  class="mb-6"
-                />
-              </div>
-            </v-scale-transition>
-          </v-sheet>
-        </v-col>
-      </v-row>
-    </v-container> -->
   </div>
 </template>
 
@@ -196,7 +93,6 @@ import { ProjectListViewModel } from '../viewmodels/project-list-viewmodel'
     'project-list-card': () => import('../components/project-list-components/project-list-card.vue'),
     'empty-list': () => import('../components/project-list-components/empty-project.vue'),
     'project-card': () => import('../components/project-list-components/project-card.vue'),
-    ConnectWallet,
   },
 })
 export default class ProjectListPage extends Vue {
