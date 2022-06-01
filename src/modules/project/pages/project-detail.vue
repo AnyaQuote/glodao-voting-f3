34df86a11fe6469b88a9264b22c9555929969020
Anh Tu <n.anhtu0101@gmail.com>
2022-06-01 15:02:58 +0700

    project detail

--- src/modules/project/pages/project-detail.vue
+++ src/modules/project/pages/project-detail.vue
@@ -1,35 +1,30 @@
 <template>
   <v-container>
-    <v-row no-gutters dense v-if="vm.poolStore" class="mb-16">
-      <!-- ---------------------------- BREADCRUMBS --------------------------------------- -->
-      <v-col cols="12" class="mt-4 mb-16">
+    <div no-gutters dense v-if="vm.poolStore" class="mb-16">
+      <div class="mt-4 mb-16">
         <div class="d-flex align-center font-weight-medium">
           <span class="app-blue--text mr-5 cursor-pointer" @click="$router.push('/projects')">Your project</span>
           <v-icon class="mr-5" size="22">mdi-chevron-right</v-icon>
           <span class="neutral10--text">{{ vm.poolStore.projectName }}</span>
         </div>
-      </v-col>
+      </div>
 
-      <!-- ---------------------------- LOADING SKELETON --------------------------------------- -->
-      <v-col cols="12" v-if="vm.loading">
+      <div v-if="vm.loading">
         <div class="row">
           <v-skeleton-loader class="col-12 mb-4" type="image" height="124" />
           <v-skeleton-loader v-for="i in 3" :key="i" class="col-4" type="image" />
         </div>
-      </v-col>
+      </div>
 
-      <!--  ----------------------------Approved && Reject project ------------------------------>
-
-      <v-col cols="12" v-else-if="vm.poolStore.status === 'approved'">
+      <div v-else-if="vm.poolStore.status === 'approved'">
         <others-header class="mb-8"></others-header>
         <others-content></others-content>
-      </v-col>
+      </div>
 
-      <!-- ----------------------------Voting project  ------------------------------>
-      <v-col cols="12" v-else>
-        <voting-content class="mb-8"></voting-content>
-      </v-col>
-    </v-row>
+      <div v-else>
+        <voting-content></voting-content>
+      </div>
+    </div>
     <cancel-dialog />
     <withdraw-dialog />
   </v-container>
