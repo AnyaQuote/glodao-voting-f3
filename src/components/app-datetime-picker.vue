b75c03292942f9de177a391e384db9277ce1f901
duchm22 <duchm22.se@gmail.com>
2022-06-01 07:46:12 +0700

    chore: format date

--- src/components/app-datetime-picker.vue
+++ src/components/app-datetime-picker.vue
@@ -1,70 +1,77 @@
 <template>
-  <v-sheet class="row ma-0">
-    <div class="col-12 col-sm-6 pa-0 pr-4">
-      <div class="font-weight-bold mb-2">
-        <slot name="date-label">{{ dateLabel }}</slot>
-      </div>
-      <v-menu min-width="auto" offset-y :close-on-content-click="false">
-        <template v-slot:activator="{ on }">
-          <app-text-field
-            readonly
-            v-on="on"
-            :disabled="disabled"
-            :rules="rules"
+  <v-form ref="form">
+    <v-sheet class="row ma-0">
+      <div class="col-12 col-sm-6 pa-0 pr-4">
+        <div class="font-weight-bold mb-2">
+          <slot name="date-label">{{ dateLabel }}</slot>
+        </div>
+        <v-menu min-width="auto" offset-y>
+          <template v-slot:activator="{ on }">
+            <app-text-field
+              readonly
+              v-on="on"
+              :disabled="disabled"
+              :rules="rules"
+              :value="formattedDate"
+              placeholder="DD/MM/YYYY"
+            />
+          </template>
+          <v-date-picker
+            reactive
+            color="blue-diversity"
+            :min="minDate"
+            :max="maxDate"
             :value="data.date"
-            placeholder="YYYY-MM-DD"
+            @change="onDateChange"
           />
-        </template>
-        <v-date-picker
-          reactive
-          color="blue-diversity"
-          :min="min"
-          :max="max"
-          :value="data.date"
-          @change="onDateChange"
-        />
-      </v-menu>
-    </div>
-    <v-spacer />
-    <div class="col-12 col-sm-6 pa-0">
-      <div class="font-weight-bold mb-2">
-        <slot name="time-label">{{ timeLabel }}</slot>
+        </v-menu>
       </div>
-      <v-menu min-width="auto" offset-y :close-on-content-click="false">
-        <template v-slot:activator="{ on }">
-          <app-text-field
-            readonly
-            v-on="on"
-            :disabled="disabled"
-            :rules="rules"
-            :value="data.time"
-            placeholder="hh:mm"
-          />
-        </template>
-        <v-time-picker format="24hr" color="blue-diversity" :value="data.time" @change="onTimeChange" />
-      </v-menu>
-    </div>
-  </v-sheet>
+      <div class="col-12 col-sm-6 pa-0">
+        <div class="font-weight-bold mb-2">
+          <slot name="time-label">{{ timeLabel }}</slot>
+        </div>
+        <v-menu min-width="auto" offset-y :close-on-content-click="false">
+          <template v-slot:activator="{ on }">
+            <app-text-field
+              readonly
+              v-on="on"
+              :disabled="disabled"
+              :rules="rules"
+              :value="data.time"
+              placeholder="HH:mm (24 hrs)"
+            />
+          </template>
+          <v-time-picker format="24hr" color="blue-diversity" :value="data.time" @change="onTimeChange" />
+        </v-menu>
+      </div>
+    </v-sheet>
+  </v-form>
 </template>
 
 <script lang="ts">
 import { set, isEmpty } from 'lodash-es'
 import { Observer } from 'mobx-vue'
-import { Component, Vue, Watch, Prop } from 'vue-property-decorator'
-import { getDateFromISO, getTheDateAfterISO, toISO, getTimeFromISO } from '@/helpers/date-helper'
+import { Component, Vue, Watch, Prop, Ref } from 'vue-property-decorator'
+import { getDateFromISO, toISO, getTimeFromISO } from '@/helpers/date-helper'
+import moment from 'moment'
 
 @Observer
 @Component
 export default class AppDateTimePicker extends Vue {
+  @Ref() form: any
   @Prop({ default: '' }) value!: string
-  @Prop({ default: null }) maxDate!: any
-  @Prop({ default: null }) minDate!: any
+  @Prop({ default: '' }) maxDate!: any
+  @Prop({ default: '' }) minDate!: any
   @Prop({ default: false }) disabled!: any
   @Prop({ default: 'Date' }) dateLabel!: string
   @Prop({ default: 'Time' }) timeLabel!: string
   @Prop({ default: () => [] }) rules!: any[]
 
   data = { date: '', time: '' }
+
+  get formattedDate() {
+    return this.data.date ? moment(this.data.date).format('DD/MM/YYYY') : ''
+  }
 
   mounted() {
     if (this.value) {
@@ -75,20 +82,14 @@ export default class AppDateTimePicker extends Vue {
     }
   }
 
-  get min() {
-    return this.minDate ? getTheDateAfterISO(this.minDate) : null
-  }
-
-  get max() {
-    return this.maxDate ? getDateFromISO(this.maxDate) : null
-  }
-
   onDateChange(value: any) {
     this.data = set(this.data, 'date', value)
+    this.form.validate()
   }
 
   onTimeChange(value: any) {
     this.data = set(this.data, 'time', value)
+    this.form.validate()
   }
 
   @Watch('data', { deep: true })
