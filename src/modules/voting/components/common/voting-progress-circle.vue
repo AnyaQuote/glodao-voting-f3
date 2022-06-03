0cbd84496c4f459b5ea88ffc87b5076b0880d9cc
duchm22 <duchm22.se@gmail.com>
2022-06-03 16:00:34 +0700

    style: prop for voting circle

--- src/modules/voting/components/common/voting-progress-circle.vue
+++ src/modules/voting/components/common/voting-progress-circle.vue
@@ -1,8 +1,6 @@
 <template>
   <v-progress-circular v-bind="$attrs" v-on="$listeners" :value="value" :size="size" :width="width" :rotate="rotate">
-    <slot>
-      <span :class="contentClass">{{ value }}%</span>
-    </slot>
+    <span class="text-h5 white--text">{{ value }}%</span>
   </v-progress-circular>
 </template>
 
@@ -12,7 +10,6 @@ import { Component, Vue, Prop } from 'vue-property-decorator'
 @Component
 export default class VotingProgressCircle extends Vue {
   @Prop(String) value!: string
-  @Prop({ default: 'text-h5 white--text' }) contentClass!: string
   @Prop({ default: '120' }) size!: string
   @Prop({ default: '4' }) width!: string
   @Prop({ default: '270' }) rotate!: string
