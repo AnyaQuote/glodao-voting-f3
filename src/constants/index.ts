873daac85d3a56471cfbf3a896fe84ec9c4bf144
duchm22 <duchm.se22@gmail.com>
2022-06-09 16:07:28 +0700

    chore: add URL_ENDPOINT

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,5 @@ import { FixedNumber } from '@ethersproject/bignumber'
 export const Zero = FixedNumber.from('0')
 export const HUNDRED = FixedNumber.from('100')
 export const ETHER_ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const URL_ENDPOINT = process.env.VUE_APP_API_STRAPI_ENDPOINT
