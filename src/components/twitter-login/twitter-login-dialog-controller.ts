56d8fd86d136d362b05a794c8fa5656d06b54d5e
duchm22 <duchm.se22@gmail.com>
2022-06-09 16:06:08 +0700

    chore: twitterLoginDialog controller

--- src/components/twitter-login/twitter-login-dialog-controller.ts
+++ src/components/twitter-login/twitter-login-dialog-controller.ts
@@ -1,18 +1,56 @@
+import { URL_ENDPOINT } from '@/constants'
+import { localdata } from '@/helpers/local-data'
+import { delay } from 'lodash-es'
 import { action, observable } from 'mobx'
+
+interface Config {
+  canClose?: boolean
+}
+
+const defaultConfig = {
+  canClose: false,
+}
 
 export class TwitterLoginDialogController {
   @observable show = false
+  @observable isProcessing = false
+  @observable config = defaultConfig
 
-  _resolver?: (args: any) => void
-
-  @action.bound open() {
+  @action.bound open(config?: Config) {
+    this.config = { ...this.config, ...(config || {}) }
     this.show = true
-    // return new Promise((resolve) => (this._resolver = resolve))
   }
 
   @action.bound close() {
-    // this._resolver && this._resolver(null)
+    this.config = defaultConfig
     this.show = false
+  }
+
+  @action.bound async handleTwitterLogin() {
+    this.isProcessing = true
+    const twitterLoginPage = `${URL_ENDPOINT}connect/twitter`
+    window.open(twitterLoginPage, '_blank')
+
+    // Watch values in local storage real time using interval
+    // This help letting dialog know when will the login process finished
+    // by watching localStorage value being populated
+    const intervalId = await new Promise<NodeJS.Timer>((resolve) => {
+      const id = setInterval(() => {
+        // Resolve promise with populated local storage value
+        if (localdata.jwt && localdata.user) {
+          resolve(id)
+        }
+      }, 1000)
+    })
+
+    clearInterval(intervalId)
+    this.isProcessing = false
+    this.show = false
+
+    // Perfom current tab
+    delay(() => {
+      location.reload()
+    }, 500)
   }
 }
 
