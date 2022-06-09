c992f7d944a27d14322eeacc2d5fb6cab7561aa3
duchm22 <duchm22.se@gmail.com>
2022-06-09 07:58:32 +0700

    style: update UI

--- src/components/attach-wallet/attach-wallet-dialog.vue
+++ src/components/attach-wallet/attach-wallet-dialog.vue
@@ -1,7 +1,10 @@
 <template>
-  <v-dialog :value="controller.show" content-class="rounded" max-width="500" persistent>
+  <v-dialog :value="controller.show" content-class="rounded" max-width="500">
     <v-card>
-      <v-card-title> </v-card-title>
+      <v-card-title>
+        <v-spacer />
+        <v-icon v-if="controller.config.canClose" @click="controller.close">mdi-close</v-icon>
+      </v-card-title>
 
       <v-card-text tag="div" class="text-center">
         <v-avatar size="64">
@@ -11,7 +14,14 @@
         <div>Connect your wallet to continue:</div>
 
         <div class="d-flex align-center mt-6">
-          <app-text-field class="rounded-r-0" hide-details height="45" :value="controller.connectedAddress" readonly />
+          <app-text-field
+            class="rounded-r-0"
+            placeholder="...Waiting wallet to connect"
+            hide-details
+            height="45"
+            :value="controller.connectedAddress"
+            readonly
+          />
           <v-btn
             class="px-8 rounded-l-0"
             :class="controller.connectedAddress && 'linear-blue--bg white--text'"
@@ -26,13 +36,16 @@
         </div>
 
         <div class="mt-3">
-          <connect-wallet btnClass="fill-width" />
+          <connect-wallet height="45" btnClass="fill-width" />
         </div>
 
         <div class="neutral-10--text mt-3 font-italic">
           *This wallet is used to get reward when participating in Bounty Hunter. And you can edit this address for any
           bounty project!
         </div>
+      </v-card-text>
+      <v-card-text tag="div" v-if="controller.config.message" class="error--text text-center">
+        {{ controller.config.message }}
       </v-card-text>
     </v-card>
   </v-dialog>
