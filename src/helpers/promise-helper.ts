99ae641ad2367942db05be46473bfb8841e09d2c
duchm22 <duchm.se22@gmail.com>
2022-06-09 18:07:10 +0700

    chore: add helper function

--- src/helpers/promise-helper.ts
+++ src/helpers/promise-helper.ts
@@ -1,4 +1,21 @@
+import { walletStore } from '@/stores/wallet-store'
+import { localdata } from './local-data'
+
 export const promiseHelper = {
   delay: (miniseconds: number) => new Promise((resolve) => setTimeout(resolve, miniseconds)),
   handle: (promise: any) => promise.then((res) => [undefined, res]).catch((err) => [err, undefined]),
+  waitForLocalStorage: () =>
+    new Promise<void>((resolve) => {
+      ;(function checkLocalStorage() {
+        if (localdata.jwt && localdata.user) resolve()
+        else setTimeout(checkLocalStorage, 1000)
+      })()
+    }),
+  waitForWalletAccount: () =>
+    new Promise<void>((resolve) => {
+      ;(function checkWalletAccount() {
+        if (walletStore.account) resolve()
+        else setTimeout(checkWalletAccount, 30)
+      })()
+    }),
 }
