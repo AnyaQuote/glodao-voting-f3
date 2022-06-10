5dbc1ff7d8998ef117d1566f375c591161dbeca1
duchm22 <duchm.se22@gmail.com>
2022-06-10 14:26:13 +0700

    chore: add constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,10 @@ export const HUNDRED = FixedNumber.from('100')
 export const ETHER_ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 export const URL_ENDPOINT = process.env.VUE_APP_API_STRAPI_ENDPOINT
+
+export const TIME_OUT_SETTING = 10000
+export const SUCCESS_STATUS = 'success'
+
+export const ERROR_MSG_TIMEOUT = 'Process took too long. Please try again.'
+export const ERROR_MSG_LOGIN_TIMEOUT = 'Login took too long to response. Please try again.'
+export const ERROR_MSG_LOGIN_TO_CONTINUE = 'Please sign in to continue to this page.'
