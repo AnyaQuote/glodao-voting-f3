864845e3d77075791be08845e35fe07bcd2d5331
duchm22 <duchm.se22@gmail.com>
2022-06-10 09:26:14 +0700

    chore: add comment for method

--- src/helpers/promise-helper.ts
+++ src/helpers/promise-helper.ts
@@ -4,6 +4,11 @@ import { localdata } from './local-data'
 export const promiseHelper = {
   delay: (miniseconds: number) => new Promise((resolve) => setTimeout(resolve, miniseconds)),
   handle: (promise: any) => promise.then((res) => [undefined, res]).catch((err) => [err, undefined]),
+  /**
+   * Set interval to watch for localStorage changes
+   * Use to notify when sign in with completed
+   * @returns resolve promise
+   */
   waitForLocalStorage: () =>
     new Promise<void>((resolve) => {
       ;(function checkLocalStorage() {
@@ -11,6 +16,12 @@ export const promiseHelper = {
         else setTimeout(checkLocalStorage, 1000)
       })()
     }),
+  /**
+   * Recursion to watch for wallet account
+   * If wallet account continue to be empty, re-call function after 30ms timeout
+   * Use to wait for wallet account value when browser reload
+   * @returns resolved promise
+   */
   waitForWalletAccount: () =>
     new Promise<void>((resolve) => {
       ;(function checkWalletAccount() {
