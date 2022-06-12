002eaca234bcfa9f07691315fc96fd4547c34722
duchm22 <duchm22.se@gmail.com>
2022-06-12 17:24:43 +0700

    chore: add message constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,9 +5,18 @@ export const ETHER_ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 export const URL_ENDPOINT = process.env.VUE_APP_API_STRAPI_ENDPOINT
 
-export const TIME_OUT_SETTING = 10000
+export const TIME_OUT_SETTING = 60000
 export const SUCCESS_STATUS = 'success'
+export const WALLET_ATTACHED_SUCCESSFUL = 'attached'
+export const WALLET_CONNECTED_SUCCESSFUL = 'connected'
 
 export const ERROR_MSG_TIMEOUT = 'Process took too long. Please try again.'
 export const ERROR_MSG_LOGIN_TIMEOUT = 'Login took too long to response. Please try again.'
 export const ERROR_MSG_LOGIN_TO_CONTINUE = 'Please sign in to continue to this page.'
+
+export const ERROR_MSG_MISSING_ATTACHED_WALLET =
+  'You have not attached any wallet yet. Please set your main wallet to continue.'
+export const ERROR_MSG_DIFFERENT_WALLET_DETECTED =
+  'Different wallet account detected. Please change back to your previous attached wallet account to continue.'
+export const ERROR_MSG_NO_WALLET_CONNECTED = 'You have not connected any wallet. Please connect your wallet to continue'
+export const MSG_RELOAD_AFTER_UPDATE_SUCCESS = 'Update successfully. Browser will reload soon...'
