4824bd4fc3dd5dc719bee903a88feee76d06caa0
duchm22 <duchm.se22@gmail.com>
2022-06-15 15:19:22 +0700

    chore: fix v-img not showing image source

--- src/components/app-file-upload.vue
+++ src/components/app-file-upload.vue
@@ -1,18 +1,11 @@
 <template>
   <v-sheet outlined class="d-flex align-center rounded px-5">
-    <div v-if="isImageFile" class="flex-shrink-0 py-5 mr-5">
-      <v-img v-if="preview.value" :src="preview.value" class="rounded" max-with="50" aspect-ratio="1"></v-img>
-      <v-img
-        v-else
-        :src="require('@/assets/icons/file-logo.svg')"
-        class="rounded"
-        max-height="42"
-        max-width="42"
-      ></v-img>
+    <div v-if="isImageFile" class="mr-5">
+      <v-img v-if="source" :src="source" class="rounded" height="50" width="50" />
+      <v-img v-else :src="require('@/assets/icons/file-logo.svg')" max-height="42" max-width="42" />
     </div>
     <v-file-input
-      v-bind="$attrs"
-      class="flex-grow-1 py-5"
+      class="py-5"
       show-size
       prepend-icon=""
       persistent-hint
@@ -20,14 +13,13 @@
       :label="config.label"
       :hint="config.hint"
       :accept="config.accept"
+      :rules="rules"
       :value="data"
       :error-messages="config.error"
       @change="onChange"
     >
       <template v-slot:selection="{ text }">
-        <span class="font-weight-bold">
-          {{ preview.name || text }}
-        </span>
+        <span class="font-weight-600"> {{ text }} </span>
       </template>
     </v-file-input>
     <div class="pl-5 py-5">
@@ -41,44 +33,20 @@
 import { checkQuizFile } from '@/helpers/file-helper'
 import { Component, Vue, Prop } from 'vue-property-decorator'
 
-type AppFile = File | string | null
-
-interface FileConfig {
-  label?: string
-  hint?: string
-  accept?: string
-  isEdit?: boolean
-  error?: string
-}
-
 @Component
 export default class AppUploadField extends Vue {
-  @Prop({ default: null }) value!: AppFile
+  @Prop({ default: null }) value!: File | null
   @Prop(Boolean) isImageFile!: boolean
   @Prop(Boolean) isQuizFile!: boolean
+  @Prop({ default: [] }) rules!: any
 
-  data: AppFile = null
-  config: FileConfig = {}
-  preview = { name: '', value: '' }
-
-  mounted() {
-    this.config = {
-      label: `Upload your ${this.isImageFile ? 'image' : 'file'}`,
-      hint: this.isImageFile ? 'Allow file types: png, jpg, svg' : 'Allowed file types: csv, txt, markdown',
-      accept: this.isImageFile ? '.png,.jpg,.svg' : '.csv,.md,.txt,.rtf',
-      isEdit: !!this.value,
-      error: '',
-    }
-
-    if (this.value && typeof this.value === 'string') {
-      this.preview = { value: this.getSource(this.value), name: this.value.split('/').pop() || '' }
-    } else if (this.value && this.value instanceof File) {
-      this.data = this.value
-
-      if (/^(image)\/.*$/.test(this.value.type)) {
-        this.preview = { ...this.preview, value: this.getSource(this.value) }
-      }
-    }
+  data = this.value
+  config: FileConfig = {
+    label: `Upload your ${this.isImageFile ? 'image' : 'file'}`,
+    hint: this.isImageFile ? 'Allow file types: png, jpg, svg' : 'Allowed file types: csv, txt, markdown',
+    accept: this.isImageFile ? '.png,.jpg,.svg' : '.csv,.md,.txt,.rtf',
+    isEdit: !!this.data,
+    error: '',
   }
 
   async onChange(event: any) {
@@ -87,20 +55,22 @@ export default class AppUploadField extends Vue {
       this.config.error = await checkQuizFile(value)
       value = this.config.error ? null : value
     }
-
-    if (this.isImageFile) {
-      this.preview.name && (this.preview.name = '')
-      this.preview.value = this.getSource(value)
-    }
     this.$emit('change', value)
   }
 
-  getSource(value: AppFile) {
-    if (value && value instanceof File && /^(image)\/.*$/.test(value.type)) {
-      return URL.createObjectURL(value)
-    } else if (value && typeof value === 'string') return value
-    return ''
+  get source() {
+    return this.data && this.data instanceof File && /^(image)\/.*$/.test(this.data.type)
+      ? URL.createObjectURL(this.data)
+      : ''
   }
+}
+
+interface FileConfig {
+  label?: string
+  hint?: string
+  accept?: string
+  isEdit?: boolean
+  error?: string
 }
 </script>
 
