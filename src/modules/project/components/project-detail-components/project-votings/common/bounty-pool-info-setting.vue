db8292fe5d9cc8dc631590c518524e470956e788
duchm22 <duchm.se22@gmail.com>
2022-06-16 12:49:30 +0700

    fix: votingStart votingEnd data display

--- src/modules/project/components/project-detail-components/project-votings/common/bounty-pool-info-setting.vue
+++ src/modules/project/components/project-detail-components/project-votings/common/bounty-pool-info-setting.vue
@@ -5,17 +5,17 @@
       <v-col cols="12" md="4" class="d-flex flex-md-column justify-space-between mb-2 mb-md-0">
         <div class="neutral-10--text mb-md-1">Total reward amount</div>
         <div class="font-weight-bold">
-          {{ vm.poolStore.requiredAmount | formatNumber }} {{ vm.poolStore.tokenName }}
+          {{ $_get(vm.poolStore, 'requiredAmount') | formatNumber }} {{ $_get(vm.poolStore, 'tokenName') }}
         </div>
       </v-col>
       <v-col cols="12" md="4" class="d-flex flex-md-column justify-space-between mb-2 mb-md-0">
         <div class="neutral-10--text mb-md-1">Total mission</div>
-        <div class="font-weight-bold">{{ vm.poolStore.totalMission }}</div>
+        <div class="font-weight-bold">{{ $_get(vm.poolStore, 'totalMission') }}</div>
       </v-col>
       <v-col cols="12" md="4" class="d-flex flex-md-column justify-space-between">
         <div class="neutral-10--text mb-md-1">Reward/mission</div>
         <div class="font-weight-bold">
-          {{ vm.poolStore.rewardPerMission | formatNumber }} {{ vm.poolStore.tokenName }}
+          {{ $_get(vm.poolStore, 'rewardPerMission') | formatNumber }} {{ $_get(vm.poolStore, 'tokenName') }}
         </div>
       </v-col>
     </v-row>
@@ -23,13 +23,14 @@
     <div class="d-flex justify-space-between justify-md-start mt-4 mt-md-6">
       <div class="pr-6">Campaign time:</div>
       <div class="d-md-flex">
-        <div class="font-weight-bold">Jan 21st, 10:00 pm</div>
-        <div
-          v-if="$vuetify.breakpoint.mdAndUp"
-          style="width: 4px; height: 4px; background: #d9d9d9 !important"
-          class="rounded-circle mx-2 align-self-center"
-        ></div>
-        <div class="font-weight-bold">Jan 21st, 10:00 pm</div>
+        <div class="font-weight-bold">{{ $_get(vm.poolStore, 'startDate') | ddmmyyyyhhmma }}</div>
+        <v-sheet
+          height="4"
+          width="4"
+          color="grey"
+          class="d-none d-sm-none d-md-block rounded-circle mx-2 align-self-center"
+        />
+        <div class="font-weight-bold">{{ $_get(vm.poolStore, 'endDate') | ddmmyyyyhhmma }}</div>
       </div>
     </div>
   </v-sheet>
--- src/modules/project/components/project-detail-components/project-votings/common/project-voting-time.vue
+++ src/modules/project/components/project-detail-components/project-votings/common/project-voting-time.vue
@@ -2,27 +2,27 @@
   <div class="rounded-lg pa-4 pa-md-6 blue-2 elevation-3">
     <div class="d-flex align-stretch mb-6">
       <div class="neutral-100 pa-1 rounded-lg d-flex justify-center mr-6 project-logo">
-        <img :src="vm.poolStore.projectLogo" height="100%" />
+        <img :src="$_get(vm.poolStore, 'projectLogo')" height="100%" />
       </div>
       <div class="spacer d-flex flex-column justify-center">
         <div
           class="text-h5 text-md-h4 font-weight-bold font-weight-bold mb-1 text-truncate"
-          :style="!$vuetify.breakpoint.mdAndUp && 'max-width: 210px'"
+          :style="!$vuetify.breakpoint.mdAndUp ? 'max-width: 210px' : ''"
         >
-          {{ vm.poolStore.projectName }}
+          {{ $_get(vm.poolStore, 'projectName') }}
         </div>
-        <div class="text-h6 text-md-h5 font-weight-bold neutral-10--text">${{ vm.poolStore.tokenName }}</div>
+        <div class="text-h6 text-md-h5 font-weight-bold neutral-10--text">${{ $_get(vm.poolStore, 'tokenName') }}</div>
       </div>
       <div></div>
     </div>
     <div>
       <div class="d-flex justify-space-between justify-md-start mb-2">
-        <span class="neutral-10--text mr-2">Voting starts: </span>
-        <span class="font-weight-bold">{{ vm.poolStore.startDate | ddmmyyyyhhmma }} </span>
+        <span class="neutral-10--text mr-2">Voting start: </span>
+        <span class="font-weight-bold">{{ $_get(vm.poolStore, 'votingStart') | ddmmyyyyhhmma }} </span>
       </div>
       <div class="d-flex justify-space-between justify-md-start">
         <span class="neutral-10--text mr-2">Voting end: </span>
-        <span class="font-weight-bold">{{ vm.poolStore.endDate | ddmmyyyyhhmma }} </span>
+        <span class="font-weight-bold">{{ $_get(vm.poolStore, 'votingEnd') | ddmmyyyyhhmma }} </span>
       </div>
     </div>
   </div>
