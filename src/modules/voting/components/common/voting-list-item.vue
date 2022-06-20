65c515f6039fa1cb249fd895f1757848d7e74da3
duchm22 <duchm.se22@gmail.com>
2022-06-20 09:45:45 +0700

    fix: voting list item responsive

--- src/modules/voting/components/common/voting-list-item.vue
+++ src/modules/voting/components/common/voting-list-item.vue
@@ -1,18 +1,21 @@
 <template>
   <v-sheet class="row no-gutters mb-2 align-center rounded pa-4" elevation="3">
     <div class="col-12 col-md-6 d-flex flex-row flex-md-column justify-space-between align-center align-md-start">
-      <div class="font-weight-bold order-last order-md-first">{{ prop.address }}</div>
+      <div class="d-none d-md-block font-weight-bold order-last order-md-first">{{ $_get(prop, 'address') }}</div>
+      <div class="d-block d-md-none font-weight-bold order-last order-md-first">
+        {{ $_get(prop, 'address') | shortAddress(5, 5) }}
+      </div>
       <div>GLD Staker</div>
     </div>
     <div
       v-if="prop.voted === 'yes'"
       class="col-12 col-md-6 d-flex flex-row justify-space-between justify-md-end align-center"
     >
-      <span class="font-weight-medium mr-4"> We want to join in this project </span>
+      <span class="font-weight-medium mr-4 text-truncate"> We want to join in this project </span>
       <v-chip color="green " class="white--text">👍 YES</v-chip>
     </div>
     <div v-else class="col-12 col-md-6 d-flex flex-row justify-space-between justify-md-end align-center">
-      <span class="font-weight-medium mr-4"> We do not want to join in this project </span>
+      <span class="font-weight-medium mr-4 text-truncate"> We don't want to join in this project </span>
       <v-chip color="red" class="white--text">👎 NO</v-chip>
     </div>
     <!-- <div class="col-12 col-md-4 d-flex flex-row flex-md-column align-md-end justify-space-between text-subtitle-2">
