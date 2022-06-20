f95275771851c5213c8ee2bfec87b1bab713a71d
duchm22 <duchm.se22@gmail.com>
2022-06-20 09:46:10 +0700

    chore: add voting ended list item UI for small screen

--- src/modules/voting/components/home/voting-ended-list-item.vue
+++ src/modules/voting/components/home/voting-ended-list-item.vue
@@ -1,55 +1,111 @@
 <template>
-  <v-sheet class="d-flex px-6 mt-6">
-    <!-- ------------------------------------- IMAGE COVER  ------------------------------------- -->
-    <v-img
-      class="d-none d-md-block rounded-lg flex-shrink-0"
-      :src="$_get(pool, 'projectCover')"
-      max-width="256"
-      height="171"
-    />
-    <!-- ------------------------------------- INFORMATION --------------------------------------- -->
-    <div class="d-flex flex-column flex-grow-1 pa-6">
-      <div class="d-flex align-center">
-        <!-- ==== LOGO ==== -->
-        <v-img :src="$_get(pool, 'projectLogo')" max-width="38" max-height="38" />
-        <span class="text-h5 font-weight-600 ml-3">{{ $_get(pool, 'projectName') }}</span>
+  <div>
+    <v-sheet class="d-none d-sm-flex px-6 mt-6">
+      <!-- ------------------------------------- IMAGE COVER  ------------------------------------- -->
+      <v-img
+        class="d-none d-md-block rounded-lg flex-shrink-0"
+        :src="$_get(pool, 'projectCover')"
+        max-width="256"
+        height="171"
+      />
+      <v-img
+        class="d-sm-block d-md-none rounded-lg flex-shrink-0 align-self-center"
+        :src="$_get(pool, 'projectCover')"
+        max-height="120"
+        aspect-ratio="1"
+      />
+
+      <!-- ------------------------------------- INFORMATION --------------------------------------- -->
+      <div class="d-flex flex-column flex-grow-1 pa-6">
+        <div class="d-flex align-center">
+          <!-- ==== LOGO ==== -->
+          <v-img :src="$_get(pool, 'projectLogo')" max-width="38" max-height="38" />
+          <span class="text-h5 font-weight-600 ml-3">{{ $_get(pool, 'projectName') }}</span>
+        </div>
+
+        <!-- ==== ENDED TIME ==== -->
+        <div class="mt-2 font-18 neutral-10--text font-weight-600">
+          Ended in {{ $_get(pool, 'votingEnd') | ddmmyyyyhhmma }}
+        </div>
+
+        <!-- ==== VOTE STAT ==== -->
+        <div class="d-flex mt-6">
+          <!-- NO STAT PERCENTAGE -->
+          <v-sheet
+            width="136"
+            outlined
+            class="d-flex text-center rounded-lg mr-2"
+            style="border-color: var(--v-green-base)"
+          >
+            <span class="flex-grow-1">{{ $_get(pool, 'votedYesPercent') | formatNumber(2) }}%</span>
+            <div class="green white--text flex-grow-1 rounded-lg">👍 YES</div>
+          </v-sheet>
+          <!-- YES STAT PERCENTAGE -->
+          <v-sheet width="136" class="d-flex text-center rounded-lg" outlined style="border-color: var(--v-red-base)">
+            <span class="flex-grow-1">{{ $_get(pool, 'votedNoPercent') | formatNumber(2) }}%</span>
+            <div class="red white--text flex-grow-1 rounded-lg">👎 NO</div>
+          </v-sheet>
+          <!-- // -->
+        </div>
+        <!-- =============== -->
       </div>
 
-      <!-- ==== ENDED TIME ==== -->
-      <div class="mt-2 font-18 neutral-10--text font-weight-600">
-        Ended in {{ $_get(pool, 'votingEnd') | ddmmyyyyhhmma }}
+      <!-- ------------------------------------- BUTTON -------------------------------------------- -->
+      <div class="align-self-center">
+        <v-btn icon class="blue-2" height="52" width="52" :to="`/voting/${pool.unicodeName}`">
+          <v-icon large>mdi-arrow-right</v-icon>
+        </v-btn>
       </div>
+      <!-- ----------------------------------------------------------------------------------------- -->
+    </v-sheet>
 
-      <!-- ==== VOTE STAT ==== -->
-      <div class="d-flex mt-6">
-        <!-- NO STAT PERCENTAGE -->
-        <v-sheet
-          width="136"
-          outlined
-          class="d-flex text-center rounded-lg mr-2"
-          style="border-color: var(--v-green-base)"
-        >
-          <span class="flex-grow-1">{{ $_get(pool, 'votedYesPercent') | formatNumber(2) }}%</span>
-          <div class="green white--text flex-grow-1 rounded-lg">👍 YES</div>
-        </v-sheet>
-        <!-- YES STAT PERCENTAGE -->
-        <v-sheet width="136" class="d-flex text-center rounded-lg" outlined style="border-color: var(--v-red-base)">
-          <span class="flex-grow-1">{{ $_get(pool, 'votedNoPercent') | formatNumber(2) }}%</span>
-          <div class="red white--text flex-grow-1 rounded-lg">👎 NO</div>
-        </v-sheet>
-        <!-- // -->
-      </div>
-      <!-- =============== -->
-    </div>
+    <v-sheet class="d-block d-sm-none mt-6">
+      <v-img class="rounded-lg" :src="$_get(pool, 'projectCover')" aspect-ratio="1" max-height="200">
+        <div class="d-flex flex-column fill-height">
+          <!-- ---------------------- UPPER LIST ITEM CONTENT START ---------------------- -->
+          <div class="d-flex justify-space-between align-start">
+            <div class="d-flex ma-1 ml-2">
+              <!-- NO STAT PERCENTAGE -->
+              <v-sheet
+                outlined
+                style="border-color: var(--v-green-base)"
+                class="d-flex transparent--bg text-center rounded-lg mr-2 text-subtitle-2 white--text"
+              >
+                <span class="flex-grow-1 px-2">{{ $_get(pool, 'votedYesPercent') | formatNumber(2) }}%</span>
+                <div class="green flex-grow-1 rounded-lg px-2">👍 YES</div>
+              </v-sheet>
+              <!-- YES STAT PERCENTAGE -->
+              <v-sheet
+                outlined
+                style="border-color: var(--v-red-base)"
+                class="d-flex text-center transparent--bg rounded-lg text-subtitle-2 white--text"
+              >
+                <span class="flex-grow-1 px-2">{{ $_get(pool, 'votedNoPercent') | formatNumber(2) }}%</span>
+                <div class="red flex-grow-1 rounded-lg px-2">👎 NO</div>
+              </v-sheet>
+            </div>
+            <v-btn icon class="blue-2 align-self-end ma-3" height="30" width="30" :to="`/voting/${pool.unicodeName}`">
+              <v-icon size="20">mdi-arrow-right</v-icon>
+            </v-btn>
+          </div>
+          <!-- ---------------------- UPPER LIST ITEM CONTENT END ---------------------- -->
 
-    <!-- ------------------------------------- BUTTON -------------------------------------------- -->
-    <div class="align-self-center">
-      <v-btn icon class="blue-2" height="52" width="52" :to="`/voting/${pool.unicodeName}`">
-        <v-icon large>mdi-arrow-right</v-icon>
-      </v-btn>
-    </div>
-    <!-- ----------------------------------------------------------------------------------------- -->
-  </v-sheet>
+          <v-spacer />
+
+          <!-- ---------------------- BOTTOM LIST ITEM CONTENT START ---------------------- -->
+          <div class="d-flex align-end px-4 pa-2 black-opaque--bg">
+            <v-img class="rounded-circle" :src="$_get(pool, 'projectLogo')" max-height="48" max-width="48" />
+            <div class="d-flex flex-column ml-4">
+              <div class="text-subtitle-1 font-weight-600 white--text">{{ $_get(pool, 'projectName') }}</div>
+              <span class="white--text">Ended in {{ $_get(pool, 'votingEnd') | ddmmyyyyhhmma }}</span>
+            </div>
+          </div>
+          <!-- ---------------------- BOTTOM LIST ITEM CONTENT END ------------------------- -->
+        </div>
+      </v-img>
+    </v-sheet>
+    <!-- ---------------------------------  MOBILE LIST ITEM VIEW END ------------------------------ -->
+  </div>
 </template>
 
 <script lang="ts">
