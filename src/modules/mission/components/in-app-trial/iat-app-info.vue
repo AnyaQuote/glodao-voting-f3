2162d9f0670fa0e69e451103533a97e57b68c928
duchm22 <duchm.se22@gmail.com>
2022-06-30 22:20:07 +0700

    feat: create app-info form
    Changes:
    - Create UI view
    - Inject viewmodel

--- src/modules/mission/components/in-app-trial/iat-app-info.vue
+++ src/modules/mission/components/in-app-trial/iat-app-info.vue
@@ -1,12 +1,95 @@
 <template>
-  <div></div>
+  <div>
+    <!-- ------------------------------------ MISSION TYPE -------------------------------------------------- -->
+    <div class="title font-weight-bold bluePrimary--text">App information</div>
+    <div class="font-weight-bold mt-4 mb-2">App title</div>
+    <app-text-field placeholder="Enter name" />
+
+    <div class="font-weight-bold mb-2">App logo</div>
+    <app-file-upload isImageFile />
+
+    <div class="d-flex flex-column flex-sm-row mt-4">
+      <div class="flex-grow">
+        <div class="mb-2 font-weight-bold">App Store</div>
+        <app-text-field placeholder="Enter link" />
+      </div>
+      <div class="d-none d-sm-block px-4" />
+      <div class="flex-grow">
+        <div class="mb-2 font-weight-bold">CH play</div>
+        <app-text-field placeholder="Enter link" />
+      </div>
+    </div>
+
+    <div class="font-weight-bold mb-2">App Description</div>
+    <app-textarea placeholder="Enter short description to describe project" />
+    <!-- ---------------------------------------------------------------------------------------------------- -->
+    <div class="font-weight-bold mb-2">App Screenshot</div>
+    <div class="d-flex mr-n4">
+      <v-sheet outlined class="py-1 dashed-border mr-4" rounded="lg" height="120" width="84">
+        <v-img width="100%" height="100%" contain src="@/assets/images/mission/mock-screenshot.jpg">
+          <v-icon class="d-flex fill-height" color="neutral-20">mdi-file-image</v-icon>
+        </v-img>
+      </v-sheet>
+      <v-sheet outlined class="py-1 dashed-border mr-4" rounded="lg" height="120" width="84">
+        <v-img height="100%" width="100%" contain>
+          <v-icon class="d-flex fill-height" color="neutral-20">mdi-file-image</v-icon>
+        </v-img>
+      </v-sheet>
+      <v-sheet outlined class="py-1 dashed-border mr-4" rounded="lg" height="120" width="84">
+        <v-img height="100%" width="100%" contain>
+          <v-icon class="d-flex fill-height" color="neutral-20">mdi-file-image</v-icon>
+        </v-img>
+      </v-sheet>
+      <v-sheet outlined class="py-1 dashed-border mr-4" rounded="lg" height="120" width="84">
+        <v-img height="100%" width="100%" contain>
+          <v-icon class="d-flex fill-height" color="neutral-20">mdi-file-image</v-icon>
+        </v-img>
+      </v-sheet>
+      <v-sheet outlined class="py-1 dashed-border mr-4" rounded="lg" height="120" width="84">
+        <v-img height="100%" width="100%" contain>
+          <v-icon class="d-flex fill-height" color="neutral-20">mdi-file-image</v-icon>
+        </v-img>
+      </v-sheet>
+      <v-sheet outlined class="py-1 dashed-border mr-4" rounded="lg" height="120" width="84">
+        <v-img height="100%" width="100%" contain>
+          <v-icon class="d-flex fill-height" color="neutral-20">mdi-file-image-plus</v-icon>
+        </v-img>
+      </v-sheet>
+    </div>
+    <!-- ---------------------------------------------------------------------------------------------------- -->
+
+    <v-divider class="dashed-border mt-7" />
+    <div class="d-flex mt-7">
+      <v-btn class="flex-grow" depressed outlined height="40" color="neutral-10" @click="goBack">Cancel</v-btn>
+      <div class="mx-4" />
+      <v-btn class="flex-grow text-none linear-blue--bg white--text" height="40" depressed @click="nextStep"
+        >Next</v-btn
+      >
+    </div>
+    <!-- ---------------------------------------------------------------------------------------------------- -->
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Observer } from 'mobx-vue'
+import { Component, Inject, Vue } from 'vue-property-decorator'
+import { NewInAppTrialViewModel } from '../../viewmodels/new-iat-viewmodels'
 
-@Component
-export default class InAppTrialAppInfo extends Vue {}
+@Observer
+@Component({
+  components: {
+    'app-file-upload': () => import('@/components/app-file-upload.vue'),
+  },
+})
+export default class InAppTrialAppInfo extends Vue {
+  @Inject() vm!: NewInAppTrialViewModel
+  goBack() {
+    this.vm.changeStep(1)
+  }
+  nextStep() {
+    this.vm.changeStep()
+  }
+}
 </script>
 
 <style scoped></style>
