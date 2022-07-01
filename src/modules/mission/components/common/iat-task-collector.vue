6b92ae39a74c7c96758629ef48d45c356055e3b8
duchm22 <duchm.se22@gmail.com>
2022-07-01 15:47:58 +0700

    style: disabled buttong when max tasks reached

--- src/modules/mission/components/common/iat-task-collector.vue
+++ src/modules/mission/components/common/iat-task-collector.vue
@@ -1,44 +1,46 @@
 <template>
   <div>
     <!-- --------------------------- DYNAMIC TASK ------------------------------- -->
-    <div v-for="(task, index) in tasks" :key="index" class="d-flex mt-6">
-      <v-sheet width="79" height="79" class="rounded-lg blue-2 d-flex">
-        <span class="ma-auto font-weight-bold text-h4">{{ index + 1 }}</span>
-      </v-sheet>
-      <div class="flex-grow-1 ml-4">
-        <div class="mb-2 font-weight-bold d-flex justify-space-between">
-          <span>Task name</span>
-          <v-icon v-show="index !== 0" @click="removeTaskAt(index)">mdi-close</v-icon>
+    <div v-for="(task, index) in tasks" :key="index">
+      <div class="d-flex mt-6">
+        <v-sheet width="79" height="79" class="rounded-lg blue-2 d-flex">
+          <span class="ma-auto font-weight-bold text-h4">{{ index + 1 }}</span>
+        </v-sheet>
+        <div class="flex-grow-1 ml-4">
+          <div class="mb-2 font-weight-bold d-flex justify-space-between">
+            <span>Task name</span>
+            <v-icon v-show="index !== 0" @click="removeTaskAt(index)">mdi-close</v-icon>
+          </div>
+          <app-text-field
+            height="45"
+            :rules="rules"
+            placeholder="Enter task name"
+            :value="task.name"
+            @change="onTaskChange(`[${index}].name`, $event)"
+          />
         </div>
-        <app-text-field
-          height="45"
-          :rules="rules"
-          placeholder="Enter task name"
-          :value="task.name"
-          @change="onTaskChange(`[${index}].name`, $event)"
-        />
       </div>
+      <v-divider class="dashed-border mt-2" />
     </div>
-    <v-divider class="dashed-border mt-2" />
 
     <!-- --------------------------- ADD TASK BUTTON ------------------------------- -->
     <v-sheet
-      class="d-flex py-4 mt-7 align-center justify-center cursor-pointer"
+      class="d-flex py-4 mt-7 align-center justify-center"
+      :class="{ 'neutral-20--text': maxTasksReached, 'cursor-pointer': !maxTasksReached }"
       @click="addTask"
       rounded="lg"
       outlined
-      v-ripple
+      v-ripple="!maxTasksReached"
     >
-      <v-icon>mdi-plus-circle-outline</v-icon>
-      <div class="neutral-10--text text-subtitle-1 font-weight-600 line-height-1 ml-3">
-        Add more tasks. Maximum is 5 tasks
-      </div>
+      <v-icon :class="{ 'neutral-20--text': maxTasksReached }">mdi-plus-circle-outline</v-icon>
+      <div class="text-subtitle-1 font-weight-600 line-height-1 ml-3">Add more tasks. Maximum is 5 tasks</div>
     </v-sheet>
     <!-- --------------------------------------------------------------------------- -->
   </div>
 </template>
 
 <script lang="ts">
+import { MAX_IN_APP_TRIAL_TASKS } from '@/constants'
 import { set } from 'lodash'
 import { Observer } from 'mobx-vue'
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
@@ -54,7 +56,7 @@ export default class InAppTrialTaskCollector extends Vue {
   tasks: ComponentTask[] = this.value?.length ? this.value : [{ name: '' }]
 
   addTask() {
-    if (this.tasks.length < 5) {
+    if (this.tasks.length < MAX_IN_APP_TRIAL_TASKS) {
       const newTask = { name: '' }
       this.tasks = this.tasks.concat(newTask)
     }
@@ -75,6 +77,10 @@ export default class InAppTrialTaskCollector extends Vue {
       const updatedTasks = this.tasks.filter((_, index) => index !== postion)
       this.tasks = updatedTasks
     }
+  }
+
+  get maxTasksReached() {
+    return MAX_IN_APP_TRIAL_TASKS === this.tasks.length
   }
 }
 
