8bedce049c38c8b2a76871736b257fffe7787f3f
duchm22 <duchm.se22@gmail.com>
2022-07-01 15:38:53 +0700

    chore: implement component

--- src/modules/mission/components/in-app-trial/iat-task-setting.vue
+++ src/modules/mission/components/in-app-trial/iat-task-setting.vue
@@ -1,35 +1,10 @@
 <template>
   <div>
     <!-- ------------------------------------ MISSION TYPE -------------------------------------------------- -->
-    <div class="title font-weight-bold bluePrimary--text">Mission setpting</div>
+    <div class="title font-weight-bold bluePrimary--text">Mission setting</div>
 
     <!-- ---------------------------------------------------------------------------------------------------- -->
-    <div class="d-flex mt-6">
-      <v-sheet width="79" height="79" class="rounded-lg blue-2 d-flex">
-        <span class="ma-auto font-weight-bold text-h4">1</span>
-      </v-sheet>
-      <div class="flex-grow-1 ml-4">
-        <div class="mb-2 font-weight-bold">Task name</div>
-        <app-text-field height="45" placeholder="Enter task name" />
-      </div>
-    </div>
-    <v-divider class="dashed-border mt-2" />
-    <div class="d-flex mt-7">
-      <v-sheet width="79" height="79" class="rounded-lg blue-2 d-flex">
-        <span class="ma-auto font-weight-bold text-h4">2</span>
-      </v-sheet>
-      <div class="flex-grow-1 ml-4">
-        <div class="mb-2 font-weight-bold">Task name</div>
-        <app-text-field height="45" placeholder="Enter task name" />
-      </div>
-    </div>
-    <v-divider class="dashed-border mt-2" />
-    <v-sheet class="d-flex py-4 mt-7 align-center justify-center" outlined rounded="lg">
-      <v-icon>mdi-plus-circle-outline</v-icon>
-      <div class="neutral-10--text text-subtitle-1 font-weight-600 line-height-1 ml-3">
-        Add more tasks. Maximum is 5 tasks
-      </div>
-    </v-sheet>
+    <iat-task-collector :value="$_get(vm.iatInfo, 'tasks', [])" @onChange="vm.updateIatInfo('tasks', $event)" />
 
     <div class="d-flex mt-7">
       <v-btn class="flex-grow" depressed outlined height="40" color="neutral-10" @click="goBack">Cancel</v-btn>
@@ -45,10 +20,15 @@
 <script lang="ts">
 import { Observer } from 'mobx-vue'
 import { Component, Inject, Vue } from 'vue-property-decorator'
+import { component } from 'vue/types/umd'
 import { NewInAppTrialViewModel } from '../../viewmodels/new-iat-viewmodels'
 
 @Observer
-@Component
+@Component({
+  components: {
+    'iat-task-collector': () => import('../common/iat-task-collector.vue'),
+  },
+})
 export default class InAppTrialTaskSetting extends Vue {
   @Inject() vm!: NewInAppTrialViewModel
   goBack() {
