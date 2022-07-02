e37b213ba7f3108e1b074a2982a30c1d02fa0e4a
duchm22 <duchm.se22@gmail.com>
2022-07-03 01:06:18 +0700

    style: responsive

--- src/modules/mission-detail/components/in-app-trial/mission-app-setting.vue
+++ src/modules/mission-detail/components/in-app-trial/mission-app-setting.vue
@@ -3,12 +3,12 @@
     <div class="text-h6 font-weight-bold">Mission setting</div>
     <v-sheet class="pa-6 mt-2" outlined rounded="lg">
       <div class="d-flex align-center font-18">
-        <v-sheet width="40" height="40" rounded="lg" class="blue-2 d-flex">
+        <v-sheet min-width="40" height="40" rounded="lg" class="blue-2 d-flex">
           <span class="ma-auto font-weight-bold">1</span>
         </v-sheet>
-        <div class="ml-4 flex-grow-1 font-weight-bold">Log in app</div>
-        <span class="mr-2 font-weight-medium blue-diversity--text">API Link</span>
-        <v-icon color="blue-diversity">mdi-content-copy</v-icon>
+        <div class="ml-4 flex-grow-1 font-weight-bold text-truncate">Log in app</div>
+        <span class="d-none d-sm-block font-weight-medium blue-diversity--text text-truncate">API Link</span>
+        <v-icon class="ml-2" color="blue-diversity">mdi-content-copy</v-icon>
       </div>
       <div
         class="blue-diversity--text neutral-20 py-2 px-5 rounded text-truncate mt-2 font-weight-bold"
@@ -18,12 +18,12 @@
       </div>
       <v-divider class="dashed-border my-3" />
       <div class="d-flex align-center font-18">
-        <v-sheet width="40" height="40" rounded="lg" class="blue-2 d-flex">
+        <v-sheet min-width="40" height="40" rounded="lg" class="blue-2 d-flex">
           <span class="ma-auto font-weight-bold">2</span>
         </v-sheet>
-        <div class="ml-4 flex-grow-1 font-weight-bold">Start running 100m</div>
-        <span class="mr-2 font-weight-medium blue-diversity--text">API Link</span>
-        <v-icon color="blue-diversity">mdi-content-copy</v-icon>
+        <div class="ml-4 flex-grow-1 font-weight-bold text-truncate">Start running 100m</div>
+        <span class="d-none d-sm-block font-weight-medium blue-diversity--text text-truncate">API Link</span>
+        <v-icon class="ml-2" color="blue-diversity">mdi-content-copy</v-icon>
       </div>
       <div
         class="blue-diversity--text neutral-20 py-2 px-5 rounded text-truncate mt-2 font-weight-bold"
@@ -33,12 +33,12 @@
       </div>
       <v-divider class="dashed-border my-3" />
       <div class="d-flex align-center font-18">
-        <v-sheet width="40" height="40" rounded="lg" class="blue-2 d-flex">
+        <v-sheet min-width="40" height="40" rounded="lg" class="blue-2 d-flex">
           <span class="ma-auto font-weight-bold">3</span>
         </v-sheet>
-        <div class="ml-4 flex-grow-1 font-weight-bold">Share an activity</div>
-        <span class="mr-2 font-weight-medium blue-diversity--text">API Link</span>
-        <v-icon color="blue-diversity">mdi-content-copy</v-icon>
+        <div class="ml-4 flex-grow-1 font-weight-bold text-truncate">Share an activity</div>
+        <span class="d-none d-sm-block font-weight-medium blue-diversity--text text-truncate">API Link</span>
+        <v-icon class="ml-2" color="blue-diversity">mdi-content-copy</v-icon>
       </div>
       <div
         class="blue-diversity--text neutral-20 py-2 px-5 rounded text-truncate mt-2 font-weight-bold"
