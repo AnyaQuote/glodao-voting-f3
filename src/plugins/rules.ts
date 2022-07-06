c100ee2d3340968ac0557854565a02ed9b166f53
duchm22 <duchm.se22@gmail.com>
2022-07-06 09:52:23 +0700

    fix: domain check rule for url
    Allow domain regex limit from min 2 chars to max 64 chars

--- src/plugins/rules.ts
+++ src/plugins/rules.ts
@@ -17,7 +17,7 @@ export const rules = {
     !v ||
     'Wrong format',
   url: (v: string) =>
-    /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(
+    /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,64}(:[0-9]{1,5})?(\/.*)?$/.test(
       v
     ) ||
     !v ||
