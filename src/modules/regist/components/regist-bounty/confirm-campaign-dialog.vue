8125c869e307ee7fe52b917c75a614d7caf24d06
duchm22 <duchm.se22@gmail.com>
2022-07-22 02:09:57 +0700

    chore: implement rewardAmount computation

--- src/modules/regist/components/regist-bounty/confirm-campaign-dialog.vue
+++ src/modules/regist/components/regist-bounty/confirm-campaign-dialog.vue
@@ -12,7 +12,7 @@
           <span class="font-weight-600">Total reward:</span>
           <v-spacer />
           <span class="font-weight-bold neutral-0--text"
-            >{{ $_get(vm.projectInfo, 'rewardAmount') }} {{ $_get(vm.projectInfo, 'tokenName') }}</span
+            >{{ vm.rewardAmount }} {{ $_get(vm.projectInfo, 'tokenName') }}</span
           >
         </div>
 
