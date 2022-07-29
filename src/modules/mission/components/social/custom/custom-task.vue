2b50262de4f8205cfde3579562740dbad43952db
duchm22 <duchm.se22@gmail.com>
2022-07-30 00:16:05 +0700

    feat: create custom task component

---
+++ src/modules/mission/components/social/custom/custom-task.vue
@@ -0,0 +1,119 @@
+<template>
+  <expand-container
+    class="mt-4"
+    type="custom"
+    title="Custom task"
+    subtitle="Customize your task"
+    @remove="removeSetting"
+  >
+    <div class="font-18 font-weight-bold mt-2">Task name<span class="red--text">*</span></div>
+    <app-text-field
+      class="mt-2"
+      :value="taskName"
+      :rules="[$rules.required]"
+      placeholder="Enter your task name"
+      @change="updateConfig('setting.name', $event)"
+    />
+
+    <div class="font-18 font-weight-bold mt-2">Task description<span class="red--text">*</span></div>
+    <app-textarea
+      class="mt-2"
+      :value="description"
+      :rules="[$rules.required]"
+      placeholder="Describe your custom task"
+      @change="updateConfig('setting.description', $event)"
+    />
+
+    <div class="font-18 font-weight-bold mt-2">Task link<span class="red--text">*</span></div>
+    <app-text-field
+      class="mt-2"
+      :value="hiddenLink"
+      append-icon="mdi-link"
+      placeholder="Enter your link"
+      :rules="[$rules.url, $rules.required]"
+      @change="updateConfig('setting.link', $event)"
+    />
+  </expand-container>
+</template>
+
+<script lang="ts">
+import { EMPTY_STRING } from '@/constants'
+import { isNotEmpty } from '@/helpers'
+import { SocialTaskComponent, SocialTaskType } from '@/models/MissionModel'
+import { isEmpty } from 'lodash-es'
+import { set } from 'mobx'
+import { Observer } from 'mobx-vue'
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
+
+const customTaskSetting = {
+  key: 1,
+  component: SocialTaskComponent.CUSTOM_TASK,
+  setting: {
+    type: SocialTaskType.CUSTOM,
+    requiredContent: '',
+    description: '',
+    link: '',
+    name: '',
+    icon: 'website',
+    isLinkRequired: false,
+  },
+}
+
+@Observer
+@Component({
+  components: {
+    'expand-container': () => import('../../common/expand-container.vue'),
+  },
+})
+export default class CutstomTask extends Vue {
+  @Prop() inputConfig!: TaskConfig
+
+  taskConfig = isEmpty(this.inputConfig) ? customTaskSetting : this.inputConfig
+
+  updateConfig(property: string, value: string) {
+    this.taskConfig = set(this.taskConfig, property, value)
+  }
+
+  @Watch('taskConfig', { deep: true })
+  onSettingUpdated(newSetting: TaskConfig) {
+    console.log('watching')
+    if (isNotEmpty(this.taskName) && isNotEmpty(this.hiddenLink) && isNotEmpty(this.description)) {
+      console.log('emit')
+      this.$emit('change', newSetting)
+    }
+  }
+
+  removeSetting() {
+    this.$emit('remove')
+  }
+
+  get taskName() {
+    return this.taskConfig.setting?.name || EMPTY_STRING
+  }
+
+  get description() {
+    return this.taskConfig.setting?.description || EMPTY_STRING
+  }
+
+  get hiddenLink() {
+    return this.taskConfig.setting?.link || EMPTY_STRING
+  }
+}
+
+interface TaskConfig {
+  key?: number
+  component?: SocialTaskComponent
+  setting?: {
+    type?: SocialTaskType
+    requiredContent?: string
+    description?: string
+    page?: string
+    link?: string
+    name?: string
+    required?: boolean
+    isLinkRequired?: boolean
+  }
+}
+</script>
+
+<style scoped></style>
