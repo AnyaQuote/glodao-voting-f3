87888e15f0da9c11718047de27ba7b5d62bb0b53
duchm22 <duchm.se22@gmail.com>
2022-07-30 19:51:10 +0700

    chore: add helper to fetch default social setting

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,12 @@
+import {
+  COMMENT_TASK_TYPE_DEFAULT_CONFIG,
+  CUSTOM_TASK_TYPE_DEFAULT_CONFIG,
+  ERROR_MSG_SOCIAL_SETTING_DATA_NOT_FOUND,
+  ERROR_MSG_SOCIAL_SETTING_TYPE_NOT_FOUND,
+  FOLLOW_TASK_TYPE_DEFAULT_CONFIG,
+  QUOTE_TASK_TYPE_DEFAULT_CONFIG,
+} from '@/constants'
+import { SocialTaskType, SocialType } from '@/models/MissionModel'
 import { isEmpty } from 'lodash'
 
 export const generateRandomString = (
@@ -19,3 +28,43 @@ export const formatFileSize = (bytes: number, decimalPoint = 2) => {
 }
 
 export const isNotEmpty = (value) => isEmpty(value)
+
+export const getDefaultSettingConfig = (social: string, type: string, key: number) => {
+  if (social === SocialType.TELEGRAM) {
+    switch (type) {
+      case SocialTaskType.FOLLOW:
+        return { ...FOLLOW_TASK_TYPE_DEFAULT_CONFIG, key }
+      case SocialTaskType.COMMENT:
+        return { ...COMMENT_TASK_TYPE_DEFAULT_CONFIG, key }
+      default:
+        throw Error(ERROR_MSG_SOCIAL_SETTING_DATA_NOT_FOUND)
+    }
+  } else if (social === SocialType.TWITTER) {
+    switch (type) {
+      case SocialTaskType.FOLLOW:
+        return { ...FOLLOW_TASK_TYPE_DEFAULT_CONFIG, key }
+      case SocialTaskType.COMMENT:
+        return { ...COMMENT_TASK_TYPE_DEFAULT_CONFIG, key }
+      case SocialTaskType.QUOTE:
+        return { ...QUOTE_TASK_TYPE_DEFAULT_CONFIG, key }
+      default:
+        throw Error(ERROR_MSG_SOCIAL_SETTING_DATA_NOT_FOUND)
+    }
+  } else if (social === SocialType.FACEBOOK) {
+    switch (type) {
+      case SocialTaskType.FOLLOW:
+        return { ...FOLLOW_TASK_TYPE_DEFAULT_CONFIG, key }
+      default:
+        throw Error(ERROR_MSG_SOCIAL_SETTING_DATA_NOT_FOUND)
+    }
+  } else if (social === SocialType.CUSTOM) {
+    switch (type) {
+      case SocialTaskType.CUSTOM:
+        return { ...CUSTOM_TASK_TYPE_DEFAULT_CONFIG, key }
+      default:
+        throw Error(ERROR_MSG_SOCIAL_SETTING_DATA_NOT_FOUND)
+    }
+  } else {
+    throw Error(ERROR_MSG_SOCIAL_SETTING_TYPE_NOT_FOUND)
+  }
+}
