5c1831b15b051d3c82107b6cd1fc6dabc7e1d49f
duchm22 <duchm.se22@gmail.com>
2022-08-03 10:58:06 +0700

    chore: turn on connect-metamask component

--- src/modules/project/components/project-list-components/empty-project.vue
+++ src/modules/project/components/project-list-components/empty-project.vue
@@ -7,7 +7,7 @@
         investors will review and vote for your project on Voting. If your project has enough votes, it will be launched
         on Bounty Hunter or Launchpad.
       </div>
-      <!-- <connect-metamask
+      <connect-metamask
         :height="!$vuetify.breakpoint.mdAndUp ? '37' : '55'"
         :btnClass="`font-18 font-weight-bold ${$vuetify.breakpoint.mdAndUp ? 'font-18' : 'text-subtitle-1'}`"
       >
@@ -20,7 +20,7 @@
         >
           New application
         </v-btn>
-      </connect-metamask> -->
+      </connect-metamask>
 
       <v-btn
         :height="!$vuetify.breakpoint.mdAndUp ? '37' : '55'"
