fba668681e2040319953234a045137bc8d16ac7f
duchm22 <duchm.se22@gmail.com>
2022-11-17 21:52:56 +0700

    fix(app-datetime-picker2): add check min max date

--- src/components/app-datetime-picker2.vue
+++ src/components/app-datetime-picker2.vue
@@ -149,26 +149,53 @@ export default class AppDateTimePicker2 extends Vue {
 
   get mtStart(): moment.Moment | null {
     if (!this.data.startDate || !this.data.startTime) return null
-    return moment(`${this.data.startDate} ${this.data.startTime}`)
+    return moment(`${this.data.startDate} ${this.data.startTime}`, 'yyyy-MM-DD HH:mm')
   }
 
   get mtEnd(): moment.Moment | null {
     if (!this.data.endDate || !this.data.endTime) return null
-    return moment(`${this.data.endDate} ${this.data.endTime}`)
+    return moment(`${this.data.endDate} ${this.data.endTime}`, 'yyyy-MM-DD HH:mm')
+  }
+
+  get mtMin(): moment.Moment | null {
+    return this.min ? moment(this.min) : null
+  }
+
+  get mtMax(): moment.Moment | null {
+    return this.max ? moment(this.max) : null
   }
 
   get startDateError() {
     if (!this.mtStart) return ''
 
-    if (this.mtEnd && this.mtStart.isAfter(this.mtEnd)) return `Must be before ${this.mtEnd.format('DD/MM/yyyy HH:mm')}`
+    if (this.mtMin?.isAfter(this.mtStart)) {
+      return `Must be after ${this.mtMin.format('DD/MM/yyyy HH:mm')}`
+    }
+
+    if (this.mtMax?.isBefore(this.mtStart)) {
+      return `Must be before ${this.mtMax.format('DD/MM/yyyy HH:mm')}`
+    }
+
+    if (this.mtEnd?.isBefore(this.mtStart)) {
+      return `Must be before ${this.mtEnd.format('DD/MM/yyyy HH:mm')}`
+    }
     return ''
   }
 
   get endDateError() {
     if (!this.mtEnd) return ''
 
-    if (this.mtStart && this.mtEnd.isBefore(this.mtStart))
+    if (this.mtMin?.isAfter(this.mtEnd)) {
+      return `Must be after ${this.mtMin.format('DD/MM/yyyy HH:mm')}`
+    }
+
+    if (this.mtMax?.isBefore(this.mtStart)) {
+      return `Must be before ${this.mtMax.format('DD/MM/yyyy HH:mm')}`
+    }
+
+    if (this.mtStart?.isAfter(this.mtEnd)) {
       return `Must be after ${this.mtStart.format('DD/MM/yyyy HH:mm')}`
+    }
     return ''
   }
 }
