04a34ae159940cc96b8997617b990bc3dcad3732
duchm22 <duchm.se22@gmail.com>
2023-01-16 01:20:27 +0700

    refactor(401): revert

--- src/modules/error/pages/401.vue
+++ src/modules/error/pages/401.vue
@@ -1,10 +1,7 @@
 <template>
   <v-container class="fill-height">
-    <v-row>
-      <v-col>
-        <app-editor :controller="controller" />
-      </v-col>
-      <!-- <div class="text-center">
+    <div class="d-flex justify-center align-center fill-height fill-width text-center">
+      <div class="text-center">
         <div class="text-h1">401</div>
         <div class="primary--text font-weight-bold text-h6 mt-2">You are not authenticated to enter this page</div>
         <div class="mx-auto mt-2">
@@ -15,28 +12,16 @@
         </v-btn>
       </div>
     </div>
-    <div> -->
-      <v-col>123{{ controller.content }}</v-col>
-      <div><strong>Test</strong></div>
-    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
-import { AppEditorController } from '@/components/editor/app-editor.controller'
 import { RoutePaths } from '@/router'
 import { Observer } from 'mobx-vue'
 import { Component, Vue } from 'vue-property-decorator'
-
 @Observer
-@Component({
-  components: {
-    'app-editor': () => import('@/components/editor/app-editor.component.vue'),
-  },
-})
+@Component
 export default class NotAuthenticated extends Vue {
-  controller = new AppEditorController()
-
   goToVotingList() {
     this.$router.replace(RoutePaths.voting_list)
   }
